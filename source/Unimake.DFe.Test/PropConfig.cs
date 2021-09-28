e4d0c6cf2895ebc656f33a38f98982afc5d995a7
Wandrey Mundin Ferreira <wandrey@unimake.com.br>
2021-09-28 10:09:18 -0300

    XUnit: Teste unitário da consulta statuso do serviço da NFe. ID #140927

---
+++ source/Unimake.DFe.Test/PropConfig.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using Unimake.Security.Platform;
+
+namespace Unimake.DFe.Test
+{
+    /// <summary>
+    /// Propriedades com configurações diversas para serem utilizados nos testes
+    /// </summary>
+    public static class PropConfig
+    {
+        /// <summary>
+        /// Caminho do certificado digital A1
+        /// </summary>
+        private static string PathCertificadoDigital { get; set; } = @"D:\projetos\UnimakePV.pfx";
+
+        /// <summary>
+        /// Senha de uso do certificado digital A1
+        /// </summary>
+        private static string SenhaCertificadoDigital { get; set; } = "12345678";
+
+        private static X509Certificate2 CertificadoDigitalField;
+
+        /// <summary>
+        /// Certificado digital
+        /// </summary>
+        public static X509Certificate2 CertificadoDigital
+        {
+            get
+            {
+                if(CertificadoDigitalField == null)
+                {
+                    CertificadoDigitalField = new CertificadoDigital().CarregarCertificadoDigitalA1(PathCertificadoDigital, SenhaCertificadoDigital);
+                }
+
+                return CertificadoDigitalField;
+            }
+            private set => throw new Exception("Não é possível atribuir um certificado digital! Somente resgate o valor da propriedade que o certificado é definido automaticamente.");
+        }
+    }
+}
