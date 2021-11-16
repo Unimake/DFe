76951de9853d7aacea98c468663845881c2fa8ab
Fernanda <fernanda@unimake.com.br>
2021-11-16 16:15:55 -0300

    Inserido município de Itabaiana-SE para envio da NFSe. ID #142234

--- source/.NET Standard/Unimake.Business.DFe/Servicos/Enums/Enums.cs
+++ source/.NET Standard/Unimake.Business.DFe/Servicos/Enums/Enums.cs
@@ -5589,7 +5589,19 @@ namespace Unimake.Business.DFe.Servicos
         /// AVMB Soluções em TI
         /// </summary>
         [Description("AVMB Soluções em TI")]
-        AVMB
+        AVMB,
+
+        /// <summary>
+        /// WebISS
+        /// </summary>
+        [Description("WebISS")]
+        WEBISS,
+
+        /// <summary>
+        /// SIMPLISS Sistema de Informação LTDA
+        /// </summary>
+        [Description("SIMPLISS Sistema de Informação LTDA")]
+        SIMPLISS
     }
 
     #endregion
