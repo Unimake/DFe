e247841d566442b22629d257d163aad2b536eb43
Fernanda <fernanda@unimake.com.br>
2021-12-16 11:43:17 -0300

    XUnit: Criado teste unitário para NFSe para o servico de ConsultarNfseServicoPrestado, com ele vamos garantir que todas as prefeituras que tem este serviço o XML está sendo enviado para o webservice e estamos tendo um retorno. ID #142792

---
+++ source/Unimake.DFe.Test/NFSe/ConsultarNfseServicoPrestadoTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Xml;
+using Unimake.Business.DFe.Servicos;
+using Unimake.Business.DFe.Servicos.NFSe;
+using Xunit;
+
+namespace Unimake.DFe.Test.NFSe
+{
+    /// <summary>
+    /// Testar o serviço: ConsultarNfseServicoPrestado
+    /// </summary>
+    public class ConsultarNfseServicoPrestadoTest
+    {
+        /// <summary>
+        /// Monta o parâmetros, de forma dinâmica, para o cenário de testes
+        /// </summary>
+        public static IEnumerable<object[]> Parametros => TestUtility.PreparaDadosCenario("ConsultarNfseServicoPrestado");
+
+        /// <summary>
+        /// Consultar Nfse Serviço Prestado para saber se a conexão com o webservice está ocorrendo corretamente.
+        /// </summary>
+        /// <param name="tipoAmbiente">Ambiente para onde deve ser enviado o XML</param>
+        [Theory]
+        [Trait("DFe", "NFSe")]
+        [MemberData(nameof(Parametros))]
+        public void Consultar(TipoAmbiente tipoAmbiente, PadraoNFSe padraoNFSe, string versaoSchema, int codMunicipio, string nomeMunicipio)
+        {
+            var nomeXMLEnvio = "ConsultarNfseServicoPrestadoEnvio-ped-sitnfse.xml";
+            var arqXML = "..\\..\\..\\NFSe\\Resources\\" + padraoNFSe.ToString() + "\\" + versaoSchema + "\\" + nomeXMLEnvio;
+
+            Debug.Assert(File.Exists(arqXML), "Arquivo " + arqXML + " não foi localizado.");
+
+            try
+            {
+                var conteudoXML = new XmlDocument();
+                conteudoXML.Load(arqXML);
+
+                var configuracao = new Configuracao
+                {
+                    TipoDFe = TipoDFe.NFSe,
+                    CertificadoDigital = PropConfig.CertificadoDigital,
+                    TipoAmbiente = tipoAmbiente,
+                    CodigoMunicipio = codMunicipio,
+                    Servico = Servico.NFSeConsultarNfseServicoPrestado,
+                    SchemaVersao = versaoSchema
+                };
+
+                var consultarNfseServicoPrestado = new ConsultarNfseServicoPrestado(conteudoXML, configuracao);
+                consultarNfseServicoPrestado.Executar();
+            }
+            catch(Exception ex)
+            {
+                Debug.Assert(false, "Falha na hora de consumir o serviço: " + nomeMunicipio + " - IBGE: " + codMunicipio + " - Padrão: " + padraoNFSe.ToString() + " - Versão schema: " + versaoSchema + "\r\nExceção: " + ex.Message, ex.StackTrace);
+            }
+        }
+    }
+}
