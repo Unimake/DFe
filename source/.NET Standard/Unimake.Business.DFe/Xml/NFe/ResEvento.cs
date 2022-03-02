dabdc381ac2ff0078940cb2a0a4d7bf89c305bb6
Fernanda <fernanda@unimake.com.br>
2022-03-02 17:04:36 -0300

    DLL UniNFe: Criado serialização do XML Resumo de Evento da NF-e. XUnit: Criado teste unitário SerializacaoDeserializacaoTest do Resumo de Evento de NF-e. ID #143076

---
+++ source/.NET Standard/Unimake.Business.DFe/Xml/NFe/ResEvento.cs
@@ -0,0 +1,83 @@
+﻿#pragma warning disable CS1591
+
+#if INTEROP
+using System.Runtime.InteropServices;
+#endif
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+using Unimake.Business.DFe.Servicos;
+
+namespace Unimake.Business.DFe.Xml.NFe
+{
+#if INTEROP
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [ProgId("Unimake.Business.DFe.Xml.NFe.ResEvento")]
+    [ComVisible(true)]
+#endif
+    [XmlRoot("resEvento", Namespace = "http://www.portalfiscal.inf.br/nfe", IsNullable = false)]
+    public class ResEvento : XMLBase
+    {
+        [XmlAttribute(AttributeName = "versao", DataType = "token")]
+        public string Versao { get; set; }
+
+        [XmlIgnore]
+        public UFBrasil COrgao { get; set; }
+
+        [XmlElement("cOrgao")]
+        public int COrgaoField
+        {
+            get => (int)COrgao;
+            set => COrgao = (UFBrasil)Enum.Parse(typeof(UFBrasil), value.ToString());
+        }
+
+        [XmlElement("CNPJ")]
+        public string CNPJ { get; set; }
+
+        [XmlElement("CPF")]
+        public string CPF { get; set; }
+
+        [XmlElement("chNFe")]
+        public string ChNFe { get; set; }
+
+        [XmlIgnore]
+        public DateTime DhEvento { get; set; }
+
+        [XmlElement("dhEvento")]
+        public string DhEventoField
+        {
+            get => DhEvento.ToString("yyyy-MM-ddTHH:mm:sszzz");
+            set => DhEvento = DateTime.Parse(value);
+        }
+
+        [XmlElement("tpEvento")]
+        public TipoEventoNFe TpEvento { get; set; }
+
+        [XmlElement("nSeqEvento")]
+        public int NSeqEvento { get; set; }
+
+        [XmlElement("xEvento")]
+        public string XEvento { get; set; }
+
+        [XmlIgnore]
+        public DateTime DhRecbto { get; set; }
+
+        [XmlElement("dhRecbto")]
+        public string DhRecbtoField
+        {
+            get => DhRecbto.ToString("yyyy-MM-ddTHH:mm:sszzz");
+            set => DhRecbto = DateTime.Parse(value);
+        }
+
+        [XmlElement("nProt")]
+        public string NProt { get; set; }
+
+        #region ShouldSerialize
+
+        public bool ShouldSerializeCNPJ() => !string.IsNullOrWhiteSpace(CNPJ);
+        public bool ShouldSerializeCPF() => !string.IsNullOrWhiteSpace(CPF);
+
+        #endregion ShouldSerialize
+    }
+}
--- source/Unimake.DFe.Test/NFe/SerializacaoDeserializacaoTest.cs
+++ source/Unimake.DFe.Test/NFe/SerializacaoDeserializacaoTest.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Xml;
 using Unimake.Business.DFe.Servicos;
+using Unimake.Business.DFe.Utility;
 using Unimake.Business.DFe.Xml.NFe;
 using Xunit;
 
@@ -58,6 +59,26 @@ namespace Unimake.DFe.Test.NFe
             var xmlSerializado = xml.GerarXML();
 
             Debug.Assert(doc.InnerText == xmlSerializado.InnerText, "XML gerado pela DLL está diferente do conteúdo do arquivo serializado.");
+        }
+
+        /// <summary>
+        /// Testar a serialização e deserialização do XML ResEvento
+        /// </summary>
+        /// <param name="arqXML">Arquivo a ser deserializado</param>
+        [Theory]
+        [Trait("DFe", "NFe"), Trait("DFe", "NFCe")]
+        [InlineData(@"..\..\..\NFe\Resources\resEvento.xml")]
+        public void SerializacaoDeserializacaoResEvento(string arqXML)
+        {
+            Debug.Assert(File.Exists(arqXML), "Arquivo " + arqXML + " não foi localizado para a realização da serialização/deserialização.");
+
+            var doc = new XmlDocument();
+            doc.Load(arqXML);
+
+            var xml = XMLUtility.Deserializar<ResEvento>(doc);
+            var doc2 = xml.GerarXML();
+
+            Debug.Assert(doc.InnerText == doc2.InnerText, "XML gerado pela DLL está diferente do conteúdo do arquivo serializado.");
         }
     }
 }
