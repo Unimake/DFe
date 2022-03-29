a6c6d79196dffe307f81468a07853d85cb91da39
wandreymundin <wandrey@unimake.com.br>
2022-03-29 15:49:03 -0300

    Iniciada classe para consumir serviço de inclusão/exclusão de cadastro do SNSM. ID #145027

---
+++ source/.NET Standard/Unimake.Business.DFe/Servicos/SNCM/InclusaoExclusaoCadastro.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Unimake.Business.DFe.Servicos.SNCM
+{
+    internal class InclusaoExclusaoCadastro
+    {
+    }
+}
