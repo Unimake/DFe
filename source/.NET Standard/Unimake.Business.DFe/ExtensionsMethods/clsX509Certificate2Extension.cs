1f47a3b5d27fbf508c99b5e3792ab1798298cb0b
wandreymundin <wandrey@unimake.com.br>
2022-05-12 13:54:19 -0300

    INTEROP: Criado classe para trabalhar com o PIN do A3 para outras linguagens (FoxProx, XHarbour, etc...). ID #145978

--- source/.NET Standard/Unimake.Business.DFe/ExtensionsMethods/clsX509Certificate2Extension.cs
+++ source/.NET Standard/Unimake.Business.DFe/ExtensionsMethods/clsX509Certificate2Extension.cs
@@ -170,4 +170,105 @@ namespace Unimake.Business.DFe.Security
             }
         }
     }
+
+#if INTEROP
+
+    /// <summary>
+    /// Extensão da classe X509Certificate2 específica para INTEROP (Outras linguagens diferente do C#)
+    /// </summary>
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [ProgId("Unimake.Business.DFe.Security.ClsX509Certificate2ExtensionInterop")]
+    [ComVisible(true)]
+    public class ClsX509Certificate2ExtensionInterop
+    {
+        /// <summary>
+        /// Carregar o PIN do certificado A3 para que não apresente a tela para o usuário digitar.
+        /// </summary>
+        /// <param name="certificado">Certificado Digital</param>
+        /// <param name="pinPassword">O Pin Code / Senha / Password do certificado digital</param>
+        public void SetPinPrivateKey(X509Certificate2 certificado, string pinPassword)
+        {
+            if (certificado == null)
+            {
+                throw new ArgumentNullException("certificado == null!");
+            }
+
+            var key = (RSACryptoServiceProvider)certificado.PrivateKey;
+
+            var ProviderHandle = IntPtr.Zero;
+            var PinBuffer = Encoding.ASCII.GetBytes(pinPassword);
+
+            //Não é necessário descarregar o handle
+            SafeNativeMethods.Execute(() => SafeNativeMethods.CryptAcquireContext(
+                ref ProviderHandle,
+                key.CspKeyContainerInfo.KeyContainerName,
+                key.CspKeyContainerInfo.ProviderName,
+                key.CspKeyContainerInfo.ProviderType,
+                SafeNativeMethods.CryptContextFlags.Silent));
+
+            SafeNativeMethods.Execute(() => SafeNativeMethods.CryptSetProvParam(
+                ProviderHandle,
+                SafeNativeMethods.CryptParameter.KeyExchangePin,
+                PinBuffer,
+                0));
+
+            SafeNativeMethods.Execute(() => SafeNativeMethods.CertSetCertificateContextProperty(
+                certificado.Handle,
+                SafeNativeMethods.CertificateProperty.CryptoProviderHandle,
+                0,
+                ProviderHandle));
+
+            CarregarPINA3(certificado);
+        }
+
+        /// <summary>
+        /// É necessário assinar um XML para carregar o PIN para não solicitar ao usuário.
+        /// </summary>
+        /// <param name="certificado">Certificado digital que é para carregar o PIN</param>
+        private void CarregarPINA3(X509Certificate2 certificado)
+        {
+            var docTemp = new XmlDocument();
+            docTemp.LoadXml("<pinA3><xServ Id=\"ID1\">PINA3</xServ></pinA3>");
+
+            var tagAssinatura = "pinA3";
+            var tagAtributoID = "xServ";
+
+            AssinaturaDigital.Assinar(docTemp, tagAssinatura, tagAtributoID, certificado, AlgorithmType.Sha1, true, "Id");
+        }
+
+        /// <summary>
+        /// Retorna true se o certificado for do tipo A3.
+        /// </summary>
+        /// <param name="x509cert">Certificado que deverá ser validado se é A3 ou não.</param>
+        /// <returns>true = É um certificado A3</returns>
+        public bool IsA3(X509Certificate2 x509cert)
+        {
+            if (x509cert == null)
+            {
+                return false;
+            }
+
+            var result = false;
+
+            try
+            {
+                if (x509cert.PrivateKey is RSACryptoServiceProvider service)
+                {
+                    if (service.CspKeyContainerInfo.Removable &&
+                    service.CspKeyContainerInfo.HardwareDevice)
+                    {
+                        result = true;
+                    }
+                }
+            }
+            catch
+            {
+                result = false;
+            }
+
+            return result;
+        }
+    }
+
+#endif
 }
