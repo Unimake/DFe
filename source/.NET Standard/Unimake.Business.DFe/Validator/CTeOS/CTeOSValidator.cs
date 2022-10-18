6dccb50ac12ef8897c4cbabcfc38d4995d674204
Wandrey Mundin Ferreira <47141566+wandreymundin@users.noreply.github.com>
2022-10-18 17:16:17 -0300

    Criado no validador do CTeOS regras para garantir que os códigos de municípios, dos mais diversos grupos de tag, sejam informados com 7 dígitos. ID #150062

--- source/.NET Standard/Unimake.Business.DFe/Validator/CTeOS/CTeOSValidator.cs
+++ source/.NET Standard/Unimake.Business.DFe/Validator/CTeOS/CTeOSValidator.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Xml.Linq;
+﻿using System.Xml.Linq;
 using Unimake.Business.DFe.Utility;
 using Unimake.Business.DFe.Validator.Abstractions;
 using Unimake.Business.DFe.Xml.CTeOS;
+using Unimake.Exceptions;
 
 namespace Unimake.Business.DFe.Validator.CTeOS
 {
@@ -11,12 +11,6 @@ namespace Unimake.Business.DFe.Validator.CTeOS
     /// </summary>
     public class CTeOSValidator : XmlValidatorBase
     {
-        #region Private Fields
-
-        private const string permitidos = "01,1B,02,2D,2E,04,06,07,08,8B,09,10,11,13,14,15,16,17,18,20,21,22,23,24,25,26,27,28 e 55";
-
-        #endregion Private Fields
-
         #region Public Constructors
 
         /// <summary>
@@ -28,17 +22,86 @@ namespace Unimake.Business.DFe.Validator.CTeOS
             var cpf = Tag.Parent.GetValue("CPF");
             var cnpj = Tag.Parent.GetValue("CNPJ");
 
-            if(!string.IsNullOrWhiteSpace(cpf) &&
+            if (!string.IsNullOrWhiteSpace(cpf) &&
                !string.IsNullOrWhiteSpace(cnpj))
             {
-                throw new Exception("Não é permitido informar conteúdo na TAG <CPF> e <CNPJ>, filhas da TAG <auxXML>, ao mesmo tempo. Somente uma delas pode ter conteúdo.");
+                throw new ValidatorDFeException("Não é permitido informar CPF e CNPJ concomitantemente nas informações dos autorizados a fazer download do XML do CTeOS. Somente um pode ser informado." +
+                    " [TAG: <CPF> e <CNPJ> do grupo de tag <infCte><autXML>]");
             }
         })
         .ValidateTag(element => element.NameEquals(nameof(RefNF.Mod)) && element.Parent.NameEquals(nameof(RefNF)), Tag =>
         {
-            if(!permitidos.Contains(Tag.Value))
+            const string permitidos = "01,1B,02,2D,2E,04,06,07,08,8B,09,10,11,13,14,15,16,17,18,20,21,22,23,24,25,26,27,28 e 55";
+
+            if (!permitidos.Contains(Tag.Value))
             {
-                throw new Exception("Conteúdo da TAG <mod>, filha da TAG <infCteSub><tomaICMS><RefNF>, inválido! Valores aceitos: " + permitidos + ".");
+                throw new ValidatorDFeException("O modelo do documento informado nas informações da NF ou CT emitido pelo tomador é inválido. Valor informado: " + Tag.Value + " - Valores aceitos: " + permitidos +
+                    " [TAG: <mod> do grupo de tag <infCte><infCTeNorm><infCteSub><tomaICMS><RefNF>]");
+            }
+        }).ValidateTag(element => element.NameEquals(nameof(Ide.CMunEnv)) && element.Parent.NameEquals(nameof(Ide)), Tag =>
+        {
+            if (UConvert.ToInt(Tag.Value) <= 0 || Tag.Value == null)
+            {
+                throw new ValidatorDFeException("Código do município de envio do CTeOS (de onde o documento foi transmitido) não foi informado. É obrigatório informar o código IBGE do município." +
+                    " [TAG: <cMunEnv> do grupo de tag <infCte><ide>]");
+            }
+
+            if (Tag.Value.Length != 7)
+            {
+                throw new ValidatorDFeException("Código do município de envio do CTeOS (de onde o documento foi transmitido) está incorreto. Código informado deve ter 7 dígitos. Valor informado: " + Tag.Value +
+                    " [TAG: <cMunEnv> do grupo de tag <infCte><ide>]");
+            }
+        }).ValidateTag(element => element.NameEquals(nameof(Ide.CMunIni)) && element.Parent.NameEquals(nameof(Ide)), Tag =>
+        {
+            if (UConvert.ToInt(Tag.Value) <= 0 || Tag.Value == null)
+            {
+                throw new ValidatorDFeException("Código do município de início da prestação do serviço de transporte não foi informado. É obrigatório informar o código IBGE do município." +
+                    " [TAG: <cMunIni> do grupo de tag <infCte><ide>]");
+            }
+
+            if (Tag.Value.Length != 7)
+            {
+                throw new ValidatorDFeException("Código do município de início da prestação de serviço de transporte está incorreto. Código informado deve ter 7 dígitos. Valor informado: " + Tag.Value +
+                    " [TAG: <cMunIni> do grupo de tag <infCte><ide>]");
+            }
+        }).ValidateTag(element => element.NameEquals(nameof(Ide.CMunFim)) && element.Parent.NameEquals(nameof(Ide)), Tag =>
+        {
+            if (UConvert.ToInt(Tag.Value) <= 0 || Tag.Value == null)
+            {
+                throw new ValidatorDFeException("Código do município de término da prestação do serviço de transporte não foi informado. É obrigatório informar o código IBGE do município." +
+                    " [TAG: <cMunFim> do grupo de tag <infCte><ide>]");
+            }
+
+            if (Tag.Value.Length != 7)
+            {
+                throw new ValidatorDFeException("Código do município de término da prestação de serviço de transporte está incorreto. Código informado deve ter 7 dígitos. Valor informado: " + Tag.Value +
+                    " [TAG: <cMunFim> do grupo de tag <infCte><ide>]");
+            }
+        }).ValidateTag(element => element.NameEquals(nameof(EnderEmit.CMun)) && element.Parent.NameEquals(nameof(EnderEmit)), Tag =>
+        {
+            if (UConvert.ToInt(Tag.Value) <= 0 || Tag.Value == null)
+            {
+                throw new ValidatorDFeException("Código do município do emitente não foi informado. É obrigatório informar o código IBGE do município." +
+                    " [TAG: <cMun> do grupo de tag <infCte><emit><enderEmit>]");
+            }
+
+            if (Tag.Value.Length != 7)
+            {
+                throw new ValidatorDFeException("Código do município do emitente está incorreto. Código informado deve ter 7 dígitos. Valor informado: " + Tag.Value +
+                    " [TAG: <cMun> do grupo de tag <infCte><emit><enderEmit>]");
+            }
+        }).ValidateTag(element => element.NameEquals(nameof(EnderToma.CMun)) && element.Parent.NameEquals(nameof(EnderToma)), Tag =>
+        {
+            if (UConvert.ToInt(Tag.Value) <= 0 || Tag.Value == null)
+            {
+                throw new ValidatorDFeException("Código do município do tomador do serviço não foi informado. É obrigatório informar o código IBGE do município." +
+                    " [TAG: <cMun> do grupo de tag <infCte><toma><enderToma>]");
+            }
+
+            if (Tag.Value.Length != 7)
+            {
+                throw new ValidatorDFeException("Código do município do tomador do serviço está incorreto. Código informado deve ter 7 dígitos. Valor informado: " + Tag.Value +
+                    " [TAG: <cMun> do grupo de tag <infCte><toma><enderToma>]");
             }
         });
 
