0302a8143d58bc127b4a9302be26858ec253b180
Wandrey Mundin Ferreira <47141566+wandreymundin@users.noreply.github.com>
2023-07-26 17:03:53 -0300

    Criado construtores para todos os serviços de NFSe padrão BETHA para permitir passar o XML no formato string. ID #156388

--- source/.NET Standard/Unimake.Business.DFe/Servicos/NFSe/ConsultarLoteRps.cs
+++ source/.NET Standard/Unimake.Business.DFe/Servicos/NFSe/ConsultarLoteRps.cs
@@ -26,6 +26,19 @@ namespace Unimake.Business.DFe.Servicos.NFSe
         /// </summary>
         /// <param name="conteudoXML">Conteúdo do XML que será enviado para o WebService</param>
         /// <param name="configuracao">Objeto "Configuracoes" com as propriedade necessária para a execução do serviço</param>
+        public ConsultarLoteRps(string conteudoXML, Configuracao configuracao) : this()
+        {
+            var xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(conteudoXML);
+
+            Inicializar(xmlDoc, configuracao);
+        }
+
+        /// <summary>
+        /// Construtor
+        /// </summary>
+        /// <param name="conteudoXML">Conteúdo do XML que será enviado para o WebService</param>
+        /// <param name="configuracao">Objeto "Configuracoes" com as propriedade necessária para a execução do serviço</param>
         public ConsultarLoteRps(XmlDocument conteudoXML, Configuracao configuracao) : base(conteudoXML, configuracao)
         { }
     }
--- source/.NET Standard/Unimake.Business.DFe/Servicos/NFSe/ConsultarNfseFaixa.cs
+++ source/.NET Standard/Unimake.Business.DFe/Servicos/NFSe/ConsultarNfseFaixa.cs
@@ -13,13 +13,26 @@ namespace Unimake.Business.DFe.Servicos.NFSe
     [ProgId("Unimake.Business.DFe.Servicos.NFSe.ConsultarNfseFaixa")]
     [ComVisible(true)]
 #endif
-    public class ConsultarNfseFaixa: ConsultarNfse
+    public class ConsultarNfseFaixa : ConsultarNfse
     {
         /// <summary>
         /// Construtor
         /// </summary>
         public ConsultarNfseFaixa() : base()
         { }
+
+        /// <summary>
+        /// Construtor
+        /// </summary>
+        /// <param name="conteudoXML">Conteúdo do XML que será enviado para o WebService</param>
+        /// <param name="configuracao">Objeto "Configuracoes" com as propriedade necessária para a execução do serviço</param>
+        public ConsultarNfseFaixa(string conteudoXML, Configuracao configuracao) : this()
+        {
+            var xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(conteudoXML);
+
+            Inicializar(xmlDoc, configuracao);
+        }
 
         /// <summary>
         /// Construtor
