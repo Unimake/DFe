93815d05effc58418d8e1e6606d4fe35ed164c1f
lucas mateus <lucasmateus@unimake.com.br>
2023-09-25 14:00:40 -0300

    Criação das classes de serialização e desserialização para o evento 2098 EFD Reinf. ID #157743

---
+++ source/.NET Standard/Unimake.Business.DFe/Xml/EFDReinf/Reinf2098.cs
@@ -0,0 +1,76 @@
+﻿#pragma warning disable CS1591
+
+#if INTEROP
+using System.Runtime.InteropServices;
+#endif
+using System;
+using System.Xml.Serialization;
+using Unimake.Business.DFe.Servicos;
+
+namespace Unimake.Business.DFe.Xml.EFDReinf
+{
+#if INTEROP
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [ProgId("Unimake.Business.DFe.Xml.EFDReinf.Reinf2098")]
+    [ComVisible(true)]
+#endif
+    [Serializable()]
+    [XmlRoot("Reinf", Namespace = "http://www.reinf.esocial.gov.br/schemas/evtReabreEvPer/v2_01_02", IsNullable = false)]
+    public class Reinf2098 : XMLBase
+    {
+        [XmlElement("evtReabreEvPer")]
+        public EvtReabreEvPer EvtReabreEvPer { get; set; }
+
+        [XmlElement(ElementName = "Signature", Namespace = "http://www.w3.org/2000/09/xmldsig#")]
+        public Signature Signature { get; set; }
+    }
+
+#if INTEROP
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [ProgId("Unimake.Business.DFe.Xml.EFDReinf.EvtReabreEvPer")]
+    [ComVisible(true)]
+#endif
+    public class EvtReabreEvPer : ReinfEventoBase
+    {
+        [XmlElement("ideEvento")]
+        public IdeEventoReinf2098 IdeEvento { get; set; }
+
+        [XmlElement("ideContri")]
+        public IdeContri IdeContri { get; set; }
+    }
+
+#if INTEROP
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [ProgId("Unimake.Business.DFe.Xml.EFDReinf.IdeEventoReinf2098")]
+    [ComVisible(true)]
+#endif
+    public class IdeEventoReinf2098
+    {
+        [XmlIgnore]
+#if INTEROP
+        public DateTime PerApur { get; set; }
+#else
+        public DateTimeOffset PerApur { get; set; }
+#endif
+
+        [XmlElement("perApur")]
+        public string PerApurField
+        {
+            get => PerApur.ToString("yyyy-MM");
+#if INTEROP
+            set => PerApur = DateTime.Parse(value);
+#else
+            set => PerApur = DateTimeOffset.Parse(value);
+#endif
+        }
+
+        [XmlElement("tpAmb")]
+        public TipoAmbiente TpAmb { get; set; }
+
+        [XmlElement("procEmi")]
+        public ProcessoEmissaoReinf ProcEmi { get; set; }
+
+        [XmlElement("verProc")]
+        public string VerProc { get; set; }
+    }
+}
