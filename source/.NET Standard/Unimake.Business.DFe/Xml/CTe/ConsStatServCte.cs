0d8cca71d67c7514feb7e613f4820d3c3f24cd70
Wandrey Mundin Ferreira <47141566+wandreymundin@users.noreply.github.com>
2023-12-20 13:39:13 -0300

    BUG-FIX: Consulta status do CTe 4.00 em alguns computadores não estava gerando o nome da ta <consStatServCTe>corretamente, estava gerando como na versão 3.00. ID #159730

--- source/.NET Standard/Unimake.Business.DFe/Xml/CTe/ConsStatServCte.cs
+++ source/.NET Standard/Unimake.Business.DFe/Xml/CTe/ConsStatServCte.cs
@@ -60,7 +60,7 @@ namespace Unimake.Business.DFe.Xml.CTe
         /// Verifica se é ou não para gerar a tag cUF
         /// </summary>
         /// <returns>Retorna true se for para gerar a tag</returns>
-        public bool ShouldSerializeCUFField() => Convert.ToDecimal(Versao) >= 400;
+        public bool ShouldSerializeCUFField() => Versao != "3.00";
 
         #endregion
 
