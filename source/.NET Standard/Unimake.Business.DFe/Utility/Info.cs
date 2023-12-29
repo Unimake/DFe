1743ee84e20d39a278cca542b3628ed8a1ba3270
Wandrey Mundin Ferreira <47141566+wandreymundin@users.noreply.github.com>
2023-12-29 14:40:40 -0300

    INTEROP: Criado classe Info para outras linguagens. ID #159835

--- source/.NET Standard/Unimake.Business.DFe/Utility/Info.cs
+++ source/.NET Standard/Unimake.Business.DFe/Utility/Info.cs
@@ -1,10 +1,14 @@
 ﻿using System.Reflection;
+using System.Runtime.InteropServices;
 
 namespace Unimake.Business.DFe.Utility
 {
     /// <summary>
     /// Informações diversas sobre a DLL
     /// </summary>
+#if INTEROP
+    [ComVisible(false)]
+#endif
     public static class Info
     {
         /// <summary>
@@ -12,4 +16,22 @@ namespace Unimake.Business.DFe.Utility
         /// </summary>
         public static string VersaoDLL => $"{Assembly.GetExecutingAssembly().GetName().Version.Major:0000}{Assembly.GetExecutingAssembly().GetName().Version.Minor:00}{Assembly.GetExecutingAssembly().GetName().Version.Build:00}{Assembly.GetExecutingAssembly().GetName().Version.Revision:0000}";
     }
+
+#if INTEROP
+
+    /// <summary>
+    /// Informações diversas sobre a DLL - INTEROP
+    /// </summary>
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [ProgId("Unimake.Business.DFe.Utility.InfoInterop")]
+    [ComVisible(true)]
+    public class InfoInterop
+    {
+        /// <summary>
+        /// Versão de compilação da DLL
+        /// </summary>
+        public static string VersaoDLL => Info.VersaoDLL;
+    }
+
+#endif
 }
