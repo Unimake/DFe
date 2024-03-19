869811c0c658f3965c0fd735db031384394af2d6
Marcelo de Souza <marcelo-unimake@users.noreply.github.com>
2024-03-19 09:17:26 -0300

    BugFix - Index was out of range. Must be non-negative and less than the size of the collection. ID #162066

---
+++ source/Unimake.DFe.Test/BugFixes/Bug162066.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using Unimake.Business.DFe.Utility;
+using Unimake.Business.DFe.Xml.NFe;
+using Xunit;
+
+namespace Unimake.DFe.Test.BugFixes
+{
+    public class Bug162066
+    {
+        #region Public Methods
+
+        [Fact]
+        public void ArgumentOutOfRangeException()
+        {
+            var file = Path.Combine(Environment.CurrentDirectory, @"BugFixes\XML\Bug162066.xml");
+            var xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(File.ReadAllText(file));
+            XMLUtility.Deserializar<ProcEventoNFe>(xmlDoc.InnerXml);
+        }
+
+        #endregion Public Methods
+    }
+}
