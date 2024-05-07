a85f6d7fae3a42076cce3fd2048bc88a3af35970
Wandrey Mundin Ferreira <47141566+wandreymundin@users.noreply.github.com>
2024-05-07 15:35:22 -0300

    Melhorias na classe que verifica conexão com a internet. ID #163405

--- source/.NET Standard/Unimake.Business.DFe/Utility/Net.cs
+++ source/.NET Standard/Unimake.Business.DFe/Utility/Net.cs
@@ -4,6 +4,7 @@
 using System.Runtime.InteropServices;
 #endif
 
+using System;
 using System.Net;
 
 namespace Unimake.Business.DFe.Utility
@@ -21,13 +22,98 @@ namespace Unimake.Business.DFe.Utility
         /// </summary>
         /// <param name="proxy">Proxy a ser utilizado para testar a conexão</param>
         /// <returns>true = Tem conexão com a internet</returns>
-        public static bool HasInternetConnection(IWebProxy proxy) => Unimake.Net.Utility.HasInternetConnection(proxy);
+        public static bool HasInternetConnection(IWebProxy proxy) => HasInternetConnection(proxy, 3);
 
         /// <summary>
         /// Verifica a conexão com a internet e retorna verdadeiro se conectado com sucesso
         /// </summary>
         /// <returns>true = Tem conexão com a internet</returns>
         public static bool HasInternetConnection() => HasInternetConnection(null);
+
+        /// <summary>
+        /// Verifica a conexão com a internet e retorna verdadeiro se conectado com sucesso
+        /// </summary>
+        /// <param name="proxy">Proxy a ser utilizado para testar a conexão</param>
+        /// <param name="timeoutInSeconds">Tempo para tentativa de conexão em segundos</param>
+        /// <param name="testUrls">URLs a serem testadas, se não informada o método utilizará 5 URLs para o teste, se uma delas funcionar, vai retornar que a conexão está ok</param>
+        /// <returns>true = Tem conexão com a internet</returns>
+        public static bool HasInternetConnection(IWebProxy proxy, int timeoutInSeconds = 3, string[] testUrls = null)
+        {
+            if (testUrls == null)
+            {
+                testUrls = new string[] {
+                    "http://clients3.google.com/generate_204",
+                    "http://www.microsoft.com",
+                    "http://www.cloudflare.com",
+                    "http://www.amazon.com",
+                    "http://www.unimake.com.br"
+                };
+            }
+
+            var retorno = true;
+
+            var timeoutMilleSeconds = (timeoutInSeconds * 1000);
+
+            foreach (var url in testUrls)
+            {
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+                if (proxy != null)
+                {
+                    httpWebRequest.Proxy = proxy;
+                }
+
+                try
+                {
+                    retorno = HasInternetConnection(httpWebRequest, timeoutMilleSeconds);
+                }
+                catch
+                {
+                    retorno = false;
+                }
+
+                if (retorno)
+                {
+                    break;
+                }
+            }
+
+            return retorno;
+        }
+
+        /// <summary>
+        /// Verifica a conexão com a internet e retorna verdadeiro se conectado com sucesso
+        /// </summary>
+        /// <param name="client">Client a ser testado</param>
+        /// <param name="timeoutInMilliSeconds">Tempo de retorno em mile segundos</param>
+        /// <returns>true=Se tudo ok com a internet</returns>
+        /// <exception cref="ArgumentNullException">Quantidade </exception>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        private static bool HasInternetConnection(HttpWebRequest client, int timeoutInMilliSeconds)
+        {
+            if (client == null)
+            {
+                throw new ArgumentNullException("client");
+            }
+
+            if (timeoutInMilliSeconds <= 0)
+            {
+                throw new ArgumentOutOfRangeException("O valor  do parâmetro 'timeoutInSeconds' deve ser maior que zero.");
+            }
+
+            try
+            {
+                client.Timeout = timeoutInMilliSeconds;
+                client.ReadWriteTimeout = timeoutInMilliSeconds;
+
+                var statusCode = (client.GetResponse() as HttpWebResponse).StatusCode;
+
+                return statusCode == HttpStatusCode.OK || statusCode == HttpStatusCode.NoContent;
+            }
+            catch
+            {
+                return false;
+            }
+        }
     }
 
 #if INTEROP
---
+++ source/Unimake.DFe.Test/Utility/NetTest.cs
@@ -0,0 +1,37 @@
+﻿using Unimake.Business.DFe.Utility;
+using Xunit;
+using static System.Net.WebRequestMethods;
+
+namespace Unimake.DFe.Test.Utility
+{
+    /// <summary>
+    /// Teste utilitário da classe Net.cs da Utility
+    /// </summary>
+    public class NetTest
+    {
+        [Fact]
+        [Trait("Utility", "Net")]
+        public void HasInternetConnectionTest()
+        {
+            string[] testUrls = new string[] {
+                "http://clients3.google.com/generate_204",
+                "http://www.microsoft.com",
+                "http://www.cloudflare.com",
+                "http://www.amazon.com",
+                "http://www.unimake.com.br"
+            };
+
+            string[] urls;
+
+            foreach (string url in testUrls)
+            {
+                urls = new string[] { url };
+                Assert.True(Business.DFe.Utility.Net.HasInternetConnection(null, 3, urls));
+            }
+
+            urls = new string[] { "http://www.unimake.com" };
+
+            Assert.False(Business.DFe.Utility.Net.HasInternetConnection(null, 3, urls));
+        }
+    }
+}
