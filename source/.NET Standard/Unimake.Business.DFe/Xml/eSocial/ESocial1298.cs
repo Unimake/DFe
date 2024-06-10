47c3e4d17cc937d3230c0adfac31bb445bf9fe80
lucas mateus <lucasmateus@unimake.com.br>
2024-06-10 10:34:38 -0300

    Criação das classes de serialização e desserialização para o evento 1298 eSocial. ID #163577

---
+++ source/.NET Standard/Unimake.Business.DFe/Xml/eSocial/ESocial1298.cs
@@ -0,0 +1,88 @@
+﻿#pragma warning disable CS1591
+
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Xml.Serialization;
+using Unimake.Business.DFe.Servicos;
+using Unimake.Business.DFe.Xml.GNRE;
+
+namespace Unimake.Business.DFe.Xml.ESocial
+{
+#if INTEROP
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [ProgId("Unimake.Business.DFe.Xml.ESocial.ESocial1298")]
+    [ComVisible(true)]
+#endif
+    [Serializable()]
+    [XmlRoot("eSocial", Namespace = "http://www.esocial.gov.br/schema/evt/evtReabreEvPer/v_S_01_02_00", IsNullable = false)]
+    public class ESocial1298 : XMLBase
+    {
+        [XmlElement("evtReabreEvPer")]
+        public EvtReabreEvPer EvtReabreEvPer { get; set; }
+    }
+
+#if INTEROP
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [ProgId("Unimake.Business.DFe.Xml.ESocial.EvtReabreEvPer")]
+    [ComVisible(true)]
+#endif
+    public class EvtReabreEvPer
+    {
+        [XmlAttribute(AttributeName = "Id", DataType = "token")]
+        public string ID { get; set; }
+
+        [XmlElement("ideEvento")]
+        public IdeEventoESocial1298 IdeEvento { get; set; }
+
+        [XmlElement("ideEmpregador")]
+        public IdeEmpregador IdeEmpregador { get; set; }
+    }
+
+#if INTEROP
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [ProgId("Unimake.Business.DFe.Xml.ESocial.IdeEventoESocial1298")]
+    [ComVisible(true)]
+#endif
+    public class IdeEventoESocial1298
+    {
+        [XmlElement("indApuracao")]
+        public IndApuracao IndApuracao { get; set; }
+
+        [XmlIgnore]
+#if INTEROP
+        public DateTime PerApur { get; set; }
+#else
+        public DateTimeOffset PerApur { get; set; }
+#endif
+
+        [XmlElement("perApur")]
+        public string PerApurField
+        {
+            get => PerApur.ToString("yyyy-MM");
+#if INTEROP
+            set => PerApur = DateTime.Parse(value);
+#else
+            set => PerApur = DateTimeOffset.Parse(value);
+#endif
+        }
+
+        [XmlElement("indGuia")]
+        public string IndGuia { get; set; }
+
+        [XmlElement("tpAmb")]
+        public TipoAmbiente TpAmb { get; set; }
+
+        [XmlElement("procEmi")]
+        public ProcEmiESocial ProcEmi { get; set; }
+
+        [XmlElement("verProc")]
+        public string VerProc { get; set; }
+
+        #region ShouldSerialize
+
+        public bool ShouldSereializeIndGuiaField() => !string.IsNullOrEmpty(IndGuia);
+
+        #endregion
+    }
+}
