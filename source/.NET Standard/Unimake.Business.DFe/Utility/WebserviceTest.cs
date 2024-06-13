3cb3f412cce9072be932cde29c9faa36f2f789eb
Wandrey Mundin Ferreira <47141566+wandreymundin@users.noreply.github.com>
2024-06-13 16:57:50 -0300

    Melhorias na classe, ainda em teste, para testar webservice da SEFAZ (Ideia é fazer um teste mais eficaz do que a consulta status do serviço). ID #164184

--- source/.NET Standard/Unimake.Business.DFe/Utility/WebserviceTest.cs
+++ source/.NET Standard/Unimake.Business.DFe/Utility/WebserviceTest.cs
@@ -80,6 +80,10 @@ namespace Unimake.Business.DFe.Utility
                 case TipoDFe.NFe:
                     resultTest = TestNFe(configuracao);
                     break;
+
+                case TipoDFe.NFCe:
+                    resultTest = TestNFCe(configuracao);
+                    break;
             }
 
             return resultTest;
@@ -100,15 +104,23 @@ namespace Unimake.Business.DFe.Utility
 
             var method = "GET";
 
-            if (configuracao.TipoDFe == TipoDFe.NFe)
+            if (UFBrasil == UFBrasil.PR)
             {
-                // Somente o Paraná é POST, demais tudo GET.
-                if (UFBrasil == UFBrasil.PR)
+                method = "POST";
+            }
+            // MT não está funcionando este TEST e tenho que gastar um tempo para entender melhor.
+            else if (UFBrasil == UFBrasil.MT)
+            {
+                return resultTest;
+            }
+
+            if (configuracao.TipoDFe == TipoDFe.NFCe)
+            {
+                if (UFBrasil == UFBrasil.MG)
                 {
-                    method = "POST";
+                    return resultTest;
                 }
-                // MT não está funcionando este TEST e tenho que gastar um tempo para entender melhor.
-                else if (UFBrasil == UFBrasil.MT)
+                if (UFBrasil == UFBrasil.CE)
                 {
                     return resultTest;
                 }
@@ -129,6 +141,8 @@ namespace Unimake.Business.DFe.Utility
 
             return resultTest;
         }
+
+        #region Testes da NFe
 
         /// <summary>
         /// Testar WebService de NFe
@@ -579,6 +593,351 @@ namespace Unimake.Business.DFe.Utility
                         }
                     }
         };
+
+        #endregion
+
+        #region Testes da NFCe
+
+        /// <summary>
+        /// Testar WebService de NFCe
+        /// </summary>
+        /// <param name="configuracao">Objeto contendo as configurações para consumir o serviço</param>
+        /// <returns>Resultado do teste</returns>
+        private ResultTest TestNFCe(Configuracao configuracao)
+        {
+            var resultTest = new ResultTest()
+            {
+                Sucess = true
+            };
+
+            for (var i = 0; i < 3; i++)
+            {
+                switch (i)
+                {
+                    case 0:
+                        configuracao.Load("StatusServico");
+                        resultTest = TestNFCeStatusServico(configuracao);
+                        goto default;
+
+                    case 1:
+                        configuracao.Load("Autorizacao");
+                        resultTest = TestNFCeAutorizacao(configuracao);
+                        goto default;
+
+                    case 2:
+                        configuracao.Load("ConsultaProtocolo");
+                        goto default;
+
+                    default:
+                        if (!resultTest.Sucess)
+                        {
+                            return resultTest;
+                        }
+                        break;
+                }
+            }
+
+            return resultTest;
+        }
+
+        /// <summary>
+        /// Testar WebService de NFCe
+        /// </summary>
+        /// <param name="configuracao">Objeto contendo as configurações para consumir o serviço</param>
+        /// <returns>Resultado do teste</returns>
+        private ResultTest TestNFCeStatusServico(Configuracao configuracao)
+        {
+            var resultTest = new ResultTest()
+            {
+                Sucess = true
+            };
+
+            resultTest = TestURL(configuracao);
+
+            if (!resultTest.Sucess)
+            {
+                return resultTest;
+            }
+
+            try
+            {
+                var xml = new Xml.NFe.ConsStatServ
+                {
+                    CUF = (UFBrasil)configuracao.CodigoUF,
+                    TpAmb = configuracao.TipoAmbiente,
+                    Versao = SchemaVersao
+                };
+
+                var statusServico = new Servicos.NFCe.StatusServico(xml, configuracao);
+                statusServico.Executar();
+
+                resultTest.CStat = statusServico.Result.CStat;
+                resultTest.XMotivo = statusServico.Result.XMotivo;
+
+                switch (statusServico.Result.CStat)
+                {
+                    case 999: //Erro não catalogado
+                    case 108: //Serviço Paralisado Momentaneamente (curto prazo)
+                    case 109: //Serviço Paralisado sem Previsão
+                        resultTest.Sucess = false;
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                resultTest.ErrorMessage = MensagemErroPadrao + Environment.NewLine + Environment.NewLine + "Exception Messages: " + ex.GetAllMessages();
+            }
+
+            return resultTest;
+        }
+
+        /// <summary>
+        /// Testar WebService de NFCe
+        /// </summary>
+        /// <param name="configuracao">Objeto contendo as configurações para consumir o serviço</param>
+        /// <param name="indSinc">Testar envio síncrono?</param>
+        /// <returns>Resultado do teste</returns>
+        private ResultTest TestNFCeAutorizacao(Configuracao configuracao)
+        {
+            var resultTest = new ResultTest()
+            {
+                Sucess = true
+            };
+
+            resultTest = TestURL(configuracao);
+
+            configuracao.CSC = "HCJBIRTWGCQ3HVQN7DCA0ZY0P2NYT6FVLPJG";
+            configuracao.CSCIDToken = 1;
+
+            if (!resultTest.Sucess)
+            {
+                return resultTest;
+            }
+
+            try
+            {
+                var xml = MontaXMLEnviNFCe((UFBrasil)configuracao.CodigoUF, configuracao.TipoAmbiente);
+
+                var autorizacao = new Servicos.NFCe.Autorizacao(xml, configuracao);
+                autorizacao.Executar();
+
+                resultTest.CStat = autorizacao.Result.CStat;
+                resultTest.XMotivo = autorizacao.Result.XMotivo;
+
+                switch (autorizacao.Result.CStat)
+                {
+                    case 999: //Erro não catalogado
+                    case 108: //Serviço Paralisado Momentaneamente (curto prazo)
+                    case 109: //Serviço Paralisado sem Previsão
+                        resultTest.Sucess = false;
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                resultTest.ErrorMessage = MensagemErroPadrao + Environment.NewLine + Environment.NewLine + "Exception Messages: " + ex.GetAllMessages();
+            }
+
+            return resultTest;
+        }
+
+
+        /// <summary>
+        /// Método auxiliar para montar XML teste
+        /// </summary>
+        /// <param name="ufBrasil">UF que vai recepcionar o XML</param>
+        /// <param name="tipoAmbiente">Ambiente para o qual o XML será enviado</param>
+        /// <returns></returns>
+        private EnviNFe MontaXMLEnviNFCe(UFBrasil ufBrasil, TipoAmbiente tipoAmbiente) => new EnviNFe
+        {
+            Versao = "4.00",
+            IdLote = "000000000000001",
+            IndSinc = SimNao.Sim,
+            NFe = new List<Business.DFe.Xml.NFe.NFe> {
+                        new Business.DFe.Xml.NFe.NFe
+                        {
+                            InfNFe = new List<InfNFe> {
+                                new InfNFe
+                                {
+                                    Versao = "4.00",
+
+                                    Ide = new Ide
+                                    {
+                                        CUF = ufBrasil,
+                                        NatOp = "VENDA PRODUC.DO ESTABELEC",
+                                        Mod = ModeloDFe.NFCe,
+                                        Serie = 16,
+                                        NNF = 57964,
+                                        DhEmi = DateTime.Now,
+                                        DhSaiEnt = DateTime.Now,
+                                        TpNF = TipoOperacao.Saida,
+                                        IdDest = DestinoOperacao.OperacaoInterna,
+                                        CMunFG = 4118402,
+                                        TpImp = FormatoImpressaoDANFE.NFCe,
+                                        TpEmis = TipoEmissao.Normal,
+                                        TpAmb = tipoAmbiente,
+                                        FinNFe = FinalidadeNFe.Normal,
+                                        IndFinal = SimNao.Sim,
+                                        IndPres = IndicadorPresenca.OperacaoPresencial,
+                                        ProcEmi = ProcessoEmissao.AplicativoContribuinte,
+                                        VerProc = "TESTE 1.00",
+                                    },
+                                    Emit = new Emit
+                                    {
+                                        CNPJ = "06117473000150",
+                                        XNome = "UNIMAKE SOLUCOES CORPORATIVAS LTDA",
+                                        XFant = "UNIMAKE - PARANAVAI",
+                                        EnderEmit = new EnderEmit
+                                        {
+                                            XLgr = "RUA ANTONIO FELIPE",
+                                            Nro = "1500",
+                                            XBairro = "CENTRO",
+                                            CMun = 4118402,
+                                            XMun = "PARANAVAI",
+                                            UF = ufBrasil,
+                                            CEP = "87704030",
+                                            Fone = "04431414900"
+                                        },
+                                        IE = "9032000301",
+                                        IM = "14018",
+                                        CNAE = "6202300",
+                                        CRT = CRT.SimplesNacional
+                                    },
+                                    Dest = new Dest
+                                    {
+                                        CNPJ = "01761135000132",
+                                        XNome = "NF-E EMITIDA EM AMBIENTE DE HOMOLOGACAO - SEM VALOR FISCAL",
+                                        EnderDest = new EnderDest
+                                        {
+                                            XLgr = "AV. COMENDADOR NORBERTO MARCONDES",
+                                            Nro = "2156",
+                                            XBairro = "CENTRO",
+                                            CMun = 4104303,
+                                            XMun = "CAMPO MOURAO",
+                                            UF = UFBrasil.PR,
+                                            CEP = "87703100",
+                                            Fone = "04431414900"
+                                        },
+                                        IndIEDest = IndicadorIEDestinatario.NaoContribuinte,
+                                        Email = "testenfe@hotmail.com"
+                                    },
+                                    Det = new List<Det> {
+                                        new Det
+                                        {
+                                            NItem = 1,
+                                            Prod = new Prod
+                                            {
+                                                CProd = "01042",
+                                                CEAN = "SEM GTIN",
+                                                XProd = "NOTA FISCAL EMITIDA EM AMBIENTE DE HOMOLOGACAO - SEM VALOR FISCAL",
+                                                NCM = "84714900",
+                                                CFOP = "5101",
+                                                UCom = "LU",
+                                                QCom = 1.00m,
+                                                VUnCom = 84.9000000000M,
+                                                VProd = 84.90,
+                                                CEANTrib = "SEM GTIN",
+                                                UTrib = "LU",
+                                                QTrib = 1.00m,
+                                                VUnTrib = 84.9000000000M,
+                                                IndTot = SimNao.Sim,
+                                                XPed = "300474",
+                                                NItemPed = "1"
+                                            },
+                                            Imposto = new Imposto
+                                            {
+                                                VTotTrib = 12.63,
+                                                ICMS = new ICMS
+                                                {
+                                                    ICMSSN102 = new ICMSSN102
+                                                    {
+                                                        Orig = OrigemMercadoria.Nacional,
+                                                        CSOSN = "102"
+                                                    }
+                                                },
+                                                PIS = new PIS
+                                                {
+                                                    PISOutr = new PISOutr
+                                                    {
+                                                        CST = "99",
+                                                        VBC = 0.00,
+                                                        PPIS = 0.00,
+                                                        VPIS = 0.00
+                                                    }
+                                                },
+                                                COFINS = new COFINS
+                                                {
+                                                    COFINSOutr = new COFINSOutr
+                                                    {
+                                                        CST = "99",
+                                                        VBC = 0.00,
+                                                        PCOFINS = 0.00,
+                                                        VCOFINS = 0.00
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    },
+                                    Total = new Total
+                                    {
+                                        ICMSTot = new ICMSTot
+                                        {
+                                            VBC = 0,
+                                            VICMS = 0,
+                                            VICMSDeson = 0,
+                                            VFCP = 0,
+                                            VBCST = 0,
+                                            VST = 0,
+                                            VFCPST = 0,
+                                            VFCPSTRet = 0,
+                                            VProd = 84.90,
+                                            VFrete = 0,
+                                            VSeg = 0,
+                                            VDesc = 0,
+                                            VII = 0,
+                                            VIPI = 0,
+                                            VIPIDevol = 0,
+                                            VPIS = 0,
+                                            VCOFINS = 0,
+                                            VOutro = 0,
+                                            VNF = 84.90,
+                                            VTotTrib = 12.63
+                                        }
+                                    },
+                                    Transp = new Transp
+                                    {
+                                        ModFrete = ModalidadeFrete.SemOcorrenciaTransporte,
+                                    },
+                                    Pag = new Pag
+                                    {
+                                        DetPag = new List<DetPag>
+                                        {
+                                             new DetPag
+                                             {
+                                                 IndPag = IndicadorPagamento.PagamentoVista,
+                                                 TPag = MeioPagamento.Dinheiro,
+                                                 VPag = 84.90
+                                             }
+                                        }
+                                    },
+                                    InfAdic = new InfAdic
+                                    {
+                                        InfCpl = ";CONTROLE: 0000241197;PEDIDO(S) ATENDIDO(S): 300474;Empresa optante pelo simples nacional, conforme lei compl. 128 de 19/12/2008;Permite o aproveitamento do credito de ICMS no valor de R$ 2,40, correspondente ao percentual de 2,83% . Nos termos do Art. 23 - LC 123/2006 (Resolucoes CGSN n. 10/2007 e 53/2008);Voce pagou aproximadamente: R$ 6,69 trib. federais / R$ 5,94 trib. estaduais / R$ 0,00 trib. municipais. Fonte: IBPT/empresometro.com.br 18.2.B A3S28F;",
+                                    },
+                                    InfRespTec = new InfRespTec
+                                    {
+                                        CNPJ = "06117473000150",
+                                        XContato = "Contato teste para NFe",
+                                        Email = "testenfe@gmail.com",
+                                        Fone = "04431414900"
+                                    }
+                                }
+                            }
+                        }
+                    }
+        };
+
+        #endregion
     }
 
     /// <summary>
