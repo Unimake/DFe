21c0ad9021f58992cf8f1d47f2c341139e372574
Mauricio R Shiguemoto <Shiguemoto001@gmail.com>
2024-07-03 14:27:32 -0300

    Criação das classes de serialização e desserialização para a consulta eventos eSocial. ID #163614

---
+++ source/.NET Standard/Unimake.Business.DFe/Xml/ESocial/ConsultaEvtsTabelaESocial.cs
@@ -0,0 +1,203 @@
+﻿#pragma warning disable CS1591
+using System;
+using System.Xml;
+using System.Xml.Serialization;
+#if INTEROP
+using System.Runtime.InteropServices;
+#endif
+
+namespace Unimake.Business.DFe.Xml.ESocial
+{
+    #region Consultar Empregador
+#if INTEROP
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [ProgId("Unimake.Business.DFe.Xml.ESocial.ConsultaEvtsTabelaESocial")]
+    [ComVisible(true)]
+#endif
+    [Serializable()]
+    [XmlRoot("eSocial", Namespace = "http://www.esocial.gov.br/schema/consulta/identificadores-eventos/empregador/v1_0_0", IsNullable = false)]
+    public class ConsultarEvtsEmpregadorESocial : XMLBase
+    {
+        [XmlElement("consultaIdentificadoresEvts")]
+        public ConsultaIdentificadoresEvts ConsultaIdentificadoresEvts { get; set; }
+    }
+    #endregion Consulta Empregador
+
+    #region Consultar Tabela
+#if INTEROP
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [ProgId("Unimake.Business.DFe.Xml.ESocial.ConsultaEvtsTabelaESocial")]
+    [ComVisible(true)]
+#endif
+    [Serializable()]
+    [XmlRoot("eSocial", Namespace = "http://www.esocial.gov.br/schema/consulta/identificadores-eventos/tabela/v1_0_0", IsNullable = false)]
+    public class ConsultarEvtsTabelaESocial : XMLBase
+    {
+        [XmlElement("consultaIdentificadoresEvts")]
+        public ConsultaIdentificadoresEvts ConsultaIdentificadoresEvts { get; set; }
+    }
+    #endregion Consultar Tabela
+
+    #region Consultar Trabalhador
+#if INTEROP
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [ProgId("Unimake.Business.DFe.Xml.ESocial.ConsultaEvtsTabelaESocial")]
+    [ComVisible(true)]
+#endif
+    [Serializable()]
+    [XmlRoot("eSocial", Namespace = "http://www.esocial.gov.br/schema/consulta/identificadores-eventos/trabalhador/v1_0_0", IsNullable = false)]
+    public class ConsultarEvtsTrabalhadorESocial : XMLBase
+    {
+        [XmlElement("consultaIdentificadoresEvts")]
+        public ConsultaIdentificadoresEvts ConsultaIdentificadoresEvts { get; set; }
+    }
+    #endregion Consulta Trabalhador
+
+#if INTEROP
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [ProgId("Unimake.Business.DFe.Xml.ESocial.ConsultaIdentificadoresEvts")]
+    [ComVisible(true)]
+#endif
+    public class ConsultaIdentificadoresEvts
+    {
+        [XmlElement("ideEmpregador")]
+        public IdeEmpregador Empregador { get; set; }
+
+        [XmlElement("consultaEvtsTabela")]
+        public ConsultaEvtsTabela ConsultaEvtsTabela { get; set; }
+
+        [XmlElement("consultaEvtsTrabalhador")]
+        public ConsultaEvtsTrabalhador ConsultaEvtsTrabalhador { get; set; }
+
+        [XmlElement("consultaEvtsEmpregador")]
+        public ConsultaEvtsEmpregador ConsultaEvtsEmpregador { get; set; }
+    }
+
+    #region  Consulta Evts Tabela
+#if INTEROP
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [ProgId("Unimake.Business.DFe.Xml.ESocial.ConsultaEvtsTabela")]
+    [ComVisible(true)]
+#endif
+    public class ConsultaEvtsTabela
+    {
+        [XmlElement("tpEvt")]
+        public string TpEvt { get; set; }
+
+        [XmlElement("chEvt")]
+        public string ChEvt { get; set; }
+
+        [XmlIgnore]
+#if INTEROP
+        public DateTime DtIni {get; set; }
+#else
+        public DateTimeOffset DtIni { get; set; }
+#endif
+        [XmlElement("dtIni")]
+        public string DtIniField
+        {
+            get => DtIni.ToString("yyyy-MM-dd");
+#if INTEROP
+            set => DtIni = DateTime.Parse(value);
+#else
+            set => DtIni = DateTimeOffset.Parse(value);
+#endif
+        }
+
+        [XmlIgnore]
+#if INTEROP
+        public DateTime DtFim {get; set; }
+#else
+        public DateTimeOffset DtFim { get; set; }
+#endif
+        [XmlElement("dtFim")]
+        public string DtFimField
+        {
+            get => DtFim.ToString("yyyy-MM-dd");
+#if INTEROP
+            set => DtFim = DateTime.Parse(value);
+#else
+            set => DtFim = DateTimeOffset.Parse(value);
+#endif
+        }
+    }
+    #endregion Consulta Evts Tabela
+
+    #region Consulta Evts Trabalhador
+#if INTEROP
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [ProgId("Unimake.Business.DFe.Xml.ESocial.ConsultaEvtsTrabalhador")]
+    [ComVisible(true)]
+#endif
+    public class ConsultaEvtsTrabalhador
+    {
+        [XmlElement("cpfTrab")]
+        public string CpfTrab { get; set; }
+
+        [XmlIgnore]
+#if INTEROP
+        public DateTime DtIni {get; set; }
+#else
+        public DateTimeOffset DtIni { get; set; }
+#endif
+        [XmlElement("dtIni")]
+        public string DtIniField
+        {
+            get => DtIni.ToString("yyyy-MM-dd");
+#if INTEROP
+            set => DtIni = DateTime.Parse(value);
+#else
+            set => DtIni = DateTimeOffset.Parse(value);
+#endif
+        }
+
+        [XmlIgnore]
+#if INTEROP
+        public DateTime DtFim {get; set; }
+#else
+        public DateTimeOffset DtFim { get; set; }
+#endif
+        [XmlElement("dtFim")]
+        public string DtFimField
+        {
+            get => DtFim.ToString("yyyy-MM-dd");
+#if INTEROP
+            set => DtFim = DateTime.Parse(value);
+#else
+            set => DtFim = DateTimeOffset.Parse(value);
+#endif
+        }
+
+    }
+    #endregion
+
+    #region Consulta Evts Empregador
+#if INTEROP
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [ProgId("Unimake.Business.DFe.Xml.ESocial.ConsultaEvtsEmpregador")]
+    [ComVisible(true)]
+#endif
+    public class ConsultaEvtsEmpregador
+    {
+        [XmlElement("tpEvt")]
+        public string TpEvt { get; set; }
+
+        [XmlIgnore]
+#if INTEROP
+        public DateTime PerApur {get; set; }
+#else
+        public DateTimeOffset PerApur { get; set; }
+#endif
+        [XmlElement("perApur")]
+        public string PerApurField
+        {
+            get => PerApur.ToString("yyyy-MM");
+#if INTEROP
+            set => PerApur = DateTime.Parse(value);
+#else
+            set => PerApur = DateTimeOffset.Parse(value);
+#endif
+        }
+    }
+    #endregion Consulta Evts Empregador
+}
---
+++ source/Unimake.DFe.Test/ESocial/SerializacaoDesserializacaoConsultas.cs
@@ -0,0 +1,67 @@
+﻿using System.IO;
+using System.Xml;
+using Unimake.Business.DFe.Utility;
+using Xunit;
+
+namespace Unimake.DFe.Test.ESocial
+{
+    public class SerializacaoDesserializacaoConsultas
+    {
+        /// <summary>
+        /// Testar a serialização e desserialização da consulta de Eventos eSocial
+        /// </summary>
+        [Theory]
+        [Trait("DFe", "ESocial")]
+        [InlineData(@"..\..\..\ESocial\Resources\ConsultaEvtsTabela-esocial-considevt.xml")]
+        public void SerializacaoDesserializacaoConsultaTabelaESocial(string arqXML)
+        {
+            Assert.True(File.Exists(arqXML), "Arquivo " + arqXML + " não foi localizado para a realização da serialização/desserialização.");
+
+            var doc = new XmlDocument();
+            doc.Load(arqXML);
+
+            var xml = XMLUtility.Deserializar<Unimake.Business.DFe.Xml.ESocial.ConsultarEvtsTabelaESocial>(doc);
+            var doc2 = xml.GerarXML();
+
+            Assert.True(doc.InnerText == doc2.InnerText, "XML gerado pela DLL está diferente do conteúdo do arquivo serializado.");
+        }
+
+        /// <summary>
+        /// Testar a serialização e desserialização da consulta de Eventos eSocial
+        /// </summary>
+        [Theory]
+        [Trait("DFe", "ESocial")]
+        [InlineData(@"..\..\..\ESocial\Resources\ConsultaEvtsEmpregador-esocial-considevt.xml")]
+        public void SerializacaoDesserializacaoConsultaEmpregadorESocial(string arqXML)
+        {
+            Assert.True(File.Exists(arqXML), "Arquivo " + arqXML + " não foi localizado para a realização da serialização/desserialização.");
+
+            var doc = new XmlDocument();
+            doc.Load(arqXML);
+
+            var xml = XMLUtility.Deserializar<Unimake.Business.DFe.Xml.ESocial.ConsultarEvtsEmpregadorESocial>(doc);
+            var doc2 = xml.GerarXML();
+
+            Assert.True(doc.InnerText == doc2.InnerText, "XML gerado pela DLL está diferente do conteúdo do arquivo serializado.");
+        }
+
+        /// <summary>
+        /// Testar a serialização e desserialização da consulta de Eventos eSocial
+        /// </summary>
+        [Theory]
+        [Trait("DFe", "ESocial")]
+        [InlineData(@"..\..\..\ESocial\Resources\ConsultaEvtsTrabalhador-esocial-considevt.xml")]
+        public void SerializacaoDesserializacaoConsultaTrabalhadorESocial(string arqXML)
+        {
+            Assert.True(File.Exists(arqXML), "Arquivo " + arqXML + " não foi localizado para a realização da serialização/desserialização.");
+
+            var doc = new XmlDocument();
+            doc.Load(arqXML);
+
+            var xml = XMLUtility.Deserializar<Unimake.Business.DFe.Xml.ESocial.ConsultarEvtsTrabalhadorESocial>(doc);
+            var doc2 = xml.GerarXML();
+
+            Assert.True(doc.InnerText == doc2.InnerText, "XML gerado pela DLL está diferente do conteúdo do arquivo serializado.");
+        }
+    }
+}
