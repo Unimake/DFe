ce366d947392615d3d459a8a361c054f5cfd0246
Mauricio R Shiguemoto <Shiguemoto001@gmail.com>
2024-07-04 17:31:53 -0300

    Criação das classes de serialização e desserialização para o download de evento eSocial. ID #163615

---
+++ source/.NET Standard/Unimake.Business.DFe/Xml/ESocial/DownloadEventos.cs
@@ -0,0 +1,75 @@
+﻿#pragma warning disable CS1591
+using System;
+using System.Xml;
+using System.Xml.Serialization;
+#if INTEROP
+using System.Runtime.InteropServices;
+#endif
+
+namespace Unimake.Business.DFe.Xml.ESocial
+{
+#if INTEROP
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [ProgId("Unimake.Business.DFe.Xml.ESocial.DownloadEventosPorID")]
+    [ComVisible(true)]
+#endif
+    [Serializable()]
+    [XmlRoot("eSocial", Namespace = "http://www.esocial.gov.br/schema/download/solicitacao/id/v1_0_0", IsNullable = false)]
+    public class DownloadEventosPorID : XMLBase
+    {
+        [XmlElement("download")]
+        public Download Download {  get; set; }
+    }
+
+#if INTEROP
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [ProgId("Unimake.Business.DFe.Xml.ESocial.DownloadEventosPorNrRec")]
+    [ComVisible(true)]
+#endif
+    [Serializable()]
+    [XmlRoot("eSocial", Namespace = "http://www.esocial.gov.br/schema/download/solicitacao/nrRecibo/v1_0_0", IsNullable = false)]
+    public class DownloadEventosPorNrRec : XMLBase
+    {
+        [XmlElement("download")]
+        public Download Download { get; set; }
+    }
+
+#if INTEROP
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [ProgId("Unimake.Business.DFe.Xml.ESocial.Download")]
+    [ComVisible(true)]
+#endif
+    public class Download
+    {
+        [XmlElement("ideEmpregador")]
+        public IdeEmpregador IdeEmpregador { get; set; }
+
+        [XmlElement("solicDownloadEvtsPorId")]
+        public SolicitacaoDownloadPorId SolicitacaoDownloadPorId {  get; set; }
+
+        [XmlElement("solicDownloadEventosPorNrRecibo")]
+        public SolicitacaoDownloadPorNrRec SolicitacaoDownloadPorNrRec { get; set; }
+    }
+
+#if INTEROP
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [ProgId("Unimake.Business.DFe.Xml.ESocial.SolicitacaoDownloadPorId")]
+    [ComVisible(true)]
+#endif
+    public class SolicitacaoDownloadPorId
+    {
+        [XmlElement("id")]
+        public string Id {  get; set; }
+    }
+
+#if INTEROP
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [ProgId("Unimake.Business.DFe.Xml.ESocial.SolicitacaoDownloadPorNrRec")]
+    [ComVisible(true)]
+#endif
+    public class SolicitacaoDownloadPorNrRec
+    {
+        [XmlElement("nrRec")]
+        public string NrRec { get; set; }
+    }
+}
---
+++ source/Unimake.DFe.Test/ESocial/Resources/SerializacaoDesserealizacaoDownloads.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using System.Xml;
+using Unimake.Business.DFe.Utility;
+using Xunit;
+
+namespace Unimake.DFe.Test.ESocial
+{
+    public class SerializacaoDesserealizacaoDownloads
+    {
+        /// <summary>
+        /// Testar a serialização e desserialização do download por ID de Eventos eSocial
+        /// </summary>
+        [Theory]
+        [Trait("DFe", "ESocial")]
+        [InlineData(@"..\..\..\ESocial\Resources\DownloadEventosPorId-esocial-downevt.xml")]
+        public void SerializacaoDesserealizacaoDownloadPorID(string arqXML)
+        {
+            Assert.True(File.Exists(arqXML), "Arquivo " + arqXML + " não foi localizado para a realização da serialização/desserialização.");
+
+            var doc = new XmlDocument();
+            doc.Load(arqXML);
+
+            var xml = XMLUtility.Deserializar<Unimake.Business.DFe.Xml.ESocial.DownloadEventosPorID>(doc);
+            var doc2 = xml.GerarXML();
+
+            Assert.True(doc.InnerText == doc2.InnerText, "XML gerado pela DLL está diferente do conteúdo do arquivo serializado.");
+        }
+
+        /// <summary>
+        /// Testar a serialização e desserialização do download por NrRecibo de Eventos eSocial
+        /// </summary>
+        [Theory]
+        [Trait("DFe", "ESocial")]
+        [InlineData(@"..\..\..\ESocial\Resources\DownloadEventosPorNrRec-esocial-downevt.xml")]
+        public void SerializacaoDesserealizacaoDownloadPorNrRec(string arqXML)
+        {
+            Assert.True(File.Exists(arqXML), "Arquivo " + arqXML + " não foi localizado para a realização da serialização/desserialização.");
+
+            var doc = new XmlDocument();
+            doc.Load(arqXML);
+
+            var xml = XMLUtility.Deserializar<Unimake.Business.DFe.Xml.ESocial.DownloadEventosPorNrRec>(doc);
+            var doc2 = xml.GerarXML();
+
+            Assert.True(doc.InnerText == doc2.InnerText, "XML gerado pela DLL está diferente do conteúdo do arquivo serializado.");
+        }
+
+    }
+}
