3a594cc8fa1fa14a4f37b5d3039b06c47224574e
Wandrey Mundin Ferreira <47141566+wandreymundin@users.noreply.github.com>
2024-07-16 15:45:06 -0300

    Retornado a execução dos testes em paralelo, ficou muito lento. ID #165086

--- source/Unimake.DFe.Test/AssemblyInfo.cs
+++ source/Unimake.DFe.Test/AssemblyInfo.cs
@@ -1,5 +1,3 @@
 ﻿using Xunit;
 
 [assembly: CollectionBehavior(DisableTestParallelization = false)]
-[assembly: TestCollectionOrderer("XunitExtensions.Orderers.PriorityOrderer", "XunitExtensions")]
-[assembly: TestCaseOrderer("XunitExtensions.Orderers.PriorityOrderer", "XunitExtensions")]
