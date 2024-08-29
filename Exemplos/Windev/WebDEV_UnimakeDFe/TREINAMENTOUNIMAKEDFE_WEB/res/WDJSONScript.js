f1449443f9e94cf8e59928afbb8120597bd976e3
Wandrey Mundin Ferreira <47141566+wandreymundin@users.noreply.github.com>
2024-08-29 09:56:11 -0300

    EXEMPLOS: Novos códigos em WINDEV, aplicação WEB, utilizando a DLL. ID #166016

---
+++ Exemplos/Windev/WebDEV_UnimakeDFe/TREINAMENTOUNIMAKEDFE_WEB/res/WDJSONScript.js
@@ -0,0 +1,3 @@
+/*! 28.0.2.0 */
+/*! VersionVI: 01F280051n   */
+var clWDJSONScriptMain=function(){"use strict";var n=0;return{JSONExecute:function(n,t){if(1==arguments.length){var i=clWDAJAXMain.sRequeteSynchroneTexte(!1,"",n,_PAGE_);return clWDUtil.oEvalJSON(i,{})}clWDAJAXMain.sRequeteAsynchroneTexte(!1,"",n,function(n){t(clWDUtil.oEvalJSON(n,{}))},clWDAJAXMain.ms_nAJAXExecuteJSONExterne)},JSONExecuteExterne:function(t,i,r){var f="Requete_"+n++,u=document.createElement("script");u.src=t+(t.indexOf("?")==-1?"?":"&")+i+"="+f;u.charset="UTF-8";window[f]=function(n){r(n);clWDUtil.oSupprimeElement(u);window[f]=null};u=document.body.appendChild(u)}}}();clWDJSONScriptMain.JSONScriptExecute=clWDJSONScriptMain.JSONExecuteExterne
