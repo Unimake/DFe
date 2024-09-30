96d4fff46e719925fe95de6ceaf6db487437ecca
Wandrey Mundin Ferreira <47141566+wandreymundin@users.noreply.github.com>
2024-09-30 15:36:57 -0300

    Ignore-revision: Liberação pacote NUGET. ID #167125

--- source/Unimake.DFe.Test/DARE/ReceitasDARETest.cs
+++ source/Unimake.DFe.Test/DARE/ReceitasDARETest.cs
@@ -12,58 +12,58 @@ namespace Unimake.DFe.Test.DARE
         /// <summary>
         /// Testar o consumo da API do receitas do DARE
         /// </summary>
-        [Theory]
-        [Trait("DFe", "ESocial")]
-        [InlineData(TipoAmbiente.Producao)]
-        [InlineData(TipoAmbiente.Homologacao)]
-        public void ReceitasDare(TipoAmbiente tipoAmbiente)
-        {
-            var configuracao = new Configuracao
-            {
-                TipoDFe = TipoDFe.DARE,
-                TipoAmbiente = tipoAmbiente,
-                Servico = Servico.DAREEnvio,
-                CertificadoDigital = PropConfig.CertificadoDigital,
-                SchemaVersao = "1.031230",
-                ApiKey = "jArkFGc5dxkxGjdmVQK7FiPQ2EJQqi7J"
-            };
+        //[Theory]
+        //[Trait("DFe", "ESocial")]
+        //[InlineData(TipoAmbiente.Producao)]
+        //[InlineData(TipoAmbiente.Homologacao)]
+        //public void ReceitasDare(TipoAmbiente tipoAmbiente)
+        //{
+        //    var configuracao = new Configuracao
+        //    {
+        //        TipoDFe = TipoDFe.DARE,
+        //        TipoAmbiente = tipoAmbiente,
+        //        Servico = Servico.DAREEnvio,
+        //        CertificadoDigital = PropConfig.CertificadoDigital,
+        //        SchemaVersao = "1.031230",
+        //        ApiKey = "jArkFGc5dxkxGjdmVQK7FiPQ2EJQqi7J"
+        //    };
 
-            var RD = new Business.DFe.Xml.DARE.Receitas
-            {
+        //    var RD = new Business.DFe.Xml.DARE.Receitas
+        //    {
 
-            };
+        //    };
 
-            var Receita = new Business.DFe.Servicos.DARE.EnvioDARE(/*NÃO SEI*/, configuracao);
-            ReceitaDARE.Executar();
-        }
+        //    var Receita = new Business.DFe.Servicos.DARE.EnvioDARE(/*NÃO SEI*/, configuracao);
+        //    ReceitaDARE.Executar();
+        //}
 
-        /// <summary>
-        /// Testar o consumo da API do receitas do DARE
-        /// </summary>
-        [Theory]
-        [Trait("DFe", "ESocial")]
-        [InlineData(TipoAmbiente.Producao)]
-        [InlineData(TipoAmbiente.Homologacao)]
-        public void DAREEnvioLote(TipoAmbiente tipoAmbiente)
-        {
-            var configuracao = new Configuracao
-            {
-                TipoDFe = TipoDFe.DARE,
-                TipoAmbiente = tipoAmbiente,
-                Servico = Servico.DAREEnvio,
-                CertificadoDigital = PropConfig.CertificadoDigital,
-                SchemaVersao = "1.00",
-                ApiKey = "jArkFGc5dxkxGjdmVQK7FiPQ2EJQqi7J"
-            };
+        ///// <summary>
+        ///// Testar o consumo da API do receitas do DARE
+        ///// </summary>
+        //[Theory]
+        //[Trait("DFe", "ESocial")]
+        //[InlineData(TipoAmbiente.Producao)]
+        //[InlineData(TipoAmbiente.Homologacao)]
+        //public void DAREEnvioLote(TipoAmbiente tipoAmbiente)
+        //{
+        //    var configuracao = new Configuracao
+        //    {
+        //        TipoDFe = TipoDFe.DARE,
+        //        TipoAmbiente = tipoAmbiente,
+        //        Servico = Servico.DAREEnvio,
+        //        CertificadoDigital = PropConfig.CertificadoDigital,
+        //        SchemaVersao = "1.00",
+        //        ApiKey = "jArkFGc5dxkxGjdmVQK7FiPQ2EJQqi7J"
+        //    };
 
-            /* TEM A VER COM Receita*/ = new Business.DFe.Xml.DARE.ReceitaDARE
-            {
-                Codigo = "001",
-                CodigoServicoDARE = "101",
-                EscopoUso = "1",
-                Nome = "Receita Exemplo"
+        //    /* TEM A VER COM Receita*/ = new Business.DFe.Xml.DARE.ReceitaDARE
+        //    {
+        //        Codigo = "001",
+        //        CodigoServicoDARE = "101",
+        //        EscopoUso = "1",
+        //        Nome = "Receita Exemplo"
 
-            };
-        }
+        //    };
+        //}
     }
 }
