fe991a58b4ab7f5cc26a91e6d1a66267dd0d81e8
Shiguemoto <shiguemoto001@gmail.com>
2024-10-29 14:34:30 -0300

    Criação das classes de serialização e deserialização para consulta status serviço da NF3e. ID #167886

---
+++ source/.NET Standard/Unimake.Business.DFe/Xml/NF3e/ConsStatServ.cs
@@ -0,0 +1,27 @@
+﻿#pragma warning disable CS1591
+
+#if INTEROP
+using System.Runtime.InteropServices;
+#endif
+using System;
+using System.Xml.Serialization;
+using Unimake.Business.DFe.Servicos;
+
+namespace Unimake.Business.DFe.Xml.NF3e
+{
+#if INTEROP
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [ProgId("Unimake.Business.DFe.Xml.NF3e.ConsStatServ")]
+    [ComVisible(true)]
+#endif
+    [Serializable()]
+    [XmlRoot("consStatServNF3e", Namespace = "http://www.portalfiscal.inf.br/nf3e", IsNullable = false)]
+    public class ConsStatServ : XMLBase
+    {
+        [XmlElement("tpAmb")]
+        public TipoAmbiente TpAmb { get; set; }
+
+        [XmlElement("xServ")]
+        public string XServ { get; set; }
+    }
+}
--- source/Unimake.DFe.Test/NF3e/SerializacaoDesserializacaoTest.cs
+++ source/Unimake.DFe.Test/NF3e/SerializacaoDesserializacaoTest.cs
@@ -26,5 +26,25 @@ namespace Unimake.DFe.Test.NF3e
 
             Assert.True(doc.InnerText == doc2.InnerText, "XML gerado pela DLL está diferente do conteúdo do arquivo serializado.");
         }
+
+        /// <summary>
+        /// Testar a serialização e desserialização do XML NF3e
+        /// </summary>
+        /// <param name="arqXML">Arquivo a ser desserializado</param>
+        [Theory]
+        [Trait("DFe", "NF3e")]
+        [InlineData(@"..\..\..\NF3e\Resources\nota_energia-ped-sta.xml")]
+        public void SerializacaoDesserializacaoNF3eConsStatServ(string arqXML)
+        {
+            Assert.True(File.Exists(arqXML), "Arquivo " + arqXML + " não foi localizado para a realização da serialização/desserialização.");
+
+            var doc = new XmlDocument();
+            doc.Load(arqXML);
+
+            var xml = XMLUtility.Deserializar<Unimake.Business.DFe.Xml.NF3e.ConsStatServ>(doc);
+            var doc2 = xml.GerarXML();
+
+            Assert.True(doc.InnerText == doc2.InnerText, "XML gerado pela DLL está diferente do conteúdo do arquivo serializado.");
+        }
     }
 }
