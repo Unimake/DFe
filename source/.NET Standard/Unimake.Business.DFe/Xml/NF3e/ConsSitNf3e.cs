5415a39620db7760aee77499a58d023a67967e24
Katllen Sirotenco <katsirotenco@gmail.com>
2024-11-13 11:41:08 -0300

    Criando as classes de serialização e deserialização para consulta situação da NF3e. ID  #168321

---
+++ source/.NET Standard/Unimake.Business.DFe/Xml/NF3e/ConsSitNf3e.cs
@@ -0,0 +1,37 @@
+﻿#pragma warning disable CS1591
+
+#if INTEROP
+using System.Runtime.InteropServices;
+#endif
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+using Unimake.Business.DFe.Servicos;
+
+namespace Unimake.Business.DFe.Xml.NF3e
+{
+
+#if INTEROP
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [ProgId("Unimake.Business.DFe.Xml.NFe.ConsSitNF3e")]
+    [ComVisible(true)]
+#endif
+
+    [XmlRoot("consSitNF3e", Namespace = "http://www.portalfiscal.inf.br/nf3e", IsNullable = false)]
+    public class ConsSitNf3e : XMLBase
+    {
+        [XmlAttribute(AttributeName = "versao", DataType = "token")]
+        public string Versao { get; set; }
+
+        [XmlElement("tpAmb")]
+        public TipoAmbiente TpAmb { get; set; }
+
+        [XmlElement("xServ")]
+        public string XServ { get; set; } = "CONSULTAR";
+
+        [XmlElement("chNF3e")]
+        public string ChNF3e { get; set; }
+    }
+}
