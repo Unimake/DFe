7da3de54ac415c5d8ebf67b3b9dd9c106836a7e5
Shiguemoto <shiguemoto001@gmail.com>
2024-11-21 13:41:41 -0300

    Criação das classes de serialização e deserialização para o retEventoNF3e. ID #168389

---
+++ source/.NET Standard/Unimake.Business.DFe/Xml/NF3e/RetEventoNF3e.cs
@@ -0,0 +1,90 @@
+﻿#pragma warning disable CS1591
+
+#if INTEROP
+using System.Runtime.InteropServices;
+#endif
+using System;
+using System.Xml.Serialization;
+using Unimake.Business.DFe.Servicos;
+
+namespace Unimake.Business.DFe.Xml.NF3e
+{
+#if INTEROP
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [ProgId("Unimake.Business.DFe.Xml.NF3e.RetEventoNF3e")]
+    [ComVisible(true)]
+#endif
+    [Serializable()]
+    [XmlRoot("retEventoNF3e", Namespace = "http://www.portalfiscal.inf.br/nf3e", IsNullable = false)]
+    public class RetEventoNF3e : XMLBase
+    {
+        [XmlAttribute(AttributeName = "versao", DataType = "token")]
+        public string Versao { get; set; }
+
+        [XmlElement("infEvento")]
+        public InfEvento InfEvento { get; set; }
+
+        [XmlElement(ElementName = "Signature", Namespace = "http://www.w3.org/2000/09/xmldsig#")]
+        public Signature Signature { get; set; }
+    }
+
+#if INTEROP
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [ProgId("Unimake.Business.DFe.Xml.NF3e.InfEvento")]
+    [ComVisible(true)]
+#endif
+    public class InfEvento
+    {
+        [XmlAttribute(AttributeName = "Id", DataType = "token")]
+        public string Versao { get; set; }
+
+        [XmlElement("tpAmb")]
+        public TipoAmbiente TpAmb { get; set; }
+
+        [XmlElement("verAplic")]
+        public string VerAplic { get; set; }
+
+        [XmlElement("cOrgao")]
+        public int COrgao { get; set; }
+
+        [XmlElement("cStat")]
+        public int CStat { get; set; }
+
+        [XmlElement("xMotivo")]
+        public string XMotivo { get; set; }
+
+        [XmlElement("chNF3e")]
+        public string ChNF3e { get; set; }
+
+        [XmlElement("tpEvento")]
+        public string TpEvento { get; set; }
+
+        [XmlElement("xEvento")]
+        public string XEvento { get; set; }
+
+        [XmlElement("nSeqEvento")]
+        public string NSeqEvento { get; set; }
+
+        [XmlIgnore]
+#if INTEROP
+        public DateTime DhRegEvento { get; set; }
+#else
+        public DateTimeOffset DhRegEvento { get; set; }
+#endif
+
+        [XmlElement("dhRegEvento")]
+        public string DhRegEventoField
+        {
+            get => DhRegEvento.ToString("yyyy-MM-ddTHH:mm:sszzz"); //yyyy-MM-ddTHH:mm:sszzz
+#if INTEROP
+            set => DhRegEvento = DateTime.Parse(value);
+#else
+            set => DhRegEvento = DateTimeOffset.Parse(value);
+#endif
+        }
+
+        [XmlElement("nProt")]
+        public string NProt { get; set; }
+    }
+        
+}
