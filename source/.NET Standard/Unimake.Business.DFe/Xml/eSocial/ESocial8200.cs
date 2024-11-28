52944be0dbb156f7686ba06bbdd4f9be9596eb93
Wesley Castilho <wesley@unimake.com.br>
2024-11-28 10:37:52 -0300

    FIX: Corrigido importação do INTEROP no evento 8200 do eSocial. ID #168664

--- source/.NET Standard/Unimake.Business.DFe/Xml/eSocial/ESocial8200.cs
+++ source/.NET Standard/Unimake.Business.DFe/Xml/eSocial/ESocial8200.cs
@@ -3,6 +3,7 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Runtime.InteropServices;
 using System.Xml;
 using System.Xml.Serialization;
 using Unimake.Business.DFe.Servicos;
