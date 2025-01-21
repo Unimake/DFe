08a421b38092794f13ea33bf11c81c59f037327b
Wesley Castilho <wesley@unimake.com.br>
2025-01-21 11:42:42 -0300

    feat: Cria a pasta "Xml/NFCom" para serialização/deserialização; inicia a classe serialização/deserialização da NFCom. ID #169786

---
+++ source/.NET Standard/Unimake.Business.DFe/Xml/NFCom/NFCom.cs
@@ -0,0 +1,26 @@
+﻿#pragma warning disable CS1591
+
+#if INTEROP
+using System.Runtime.InteropServices;
+#endif
+
+using System;
+using System.Xml.Serialization;
+
+namespace Unimake.Business.DFe.Xml.NFCom
+{
+    /// <summary>
+    /// NFCom - Portal da Nota Fiscal Fatura de Serviço de Comunicação Eletrônica
+    /// </summary>
+#if INTEROP
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [ProgId("Unimake.Business.DFe.Xml.NFCom.NFCom")]
+    [ComVisible(true)]
+#endif
+    [Serializable()]
+    [XmlType(Namespace = "http://www.portalfiscal.inf.br/nfcom")]
+    [XmlRoot("NFCom", Namespace = "http://www.portalfiscal.inf.br/nfcom", IsNullable = false)]
+    public class NFCom
+    {
+    }
+}
