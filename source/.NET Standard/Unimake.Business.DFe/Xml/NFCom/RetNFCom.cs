917feaed4e559db18cfc457660e225b3a151cb1a
Wesley Castilho <wesley@unimake.com.br>
2025-01-23 09:04:52 -0300

    feat: Cria a classe de serialização/deserialização do retorno da NFCom. ID #169786

---
+++ source/.NET Standard/Unimake.Business.DFe/Xml/NFCom/RetNFCom.cs
@@ -0,0 +1,53 @@
+﻿#pragma warning disable CS1591
+
+#if INTEROP
+using System.Runtime.InteropServices;
+#endif
+
+using System;
+using System.Xml.Serialization;
+using Unimake.Business.DFe.Servicos;
+
+namespace Unimake.Business.DFe.Xml.NFCom
+{
+    /// <summary>
+    /// Classe de retorno da NFCom
+    /// </summary>
+#if INTEROP
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [ProgId("Unimake.Business.DFe.Xml.NFCom.RetNFCom")]
+    [ComVisible(true)]
+#endif
+    [Serializable()]
+    [XmlRoot("retNFCom", Namespace = "http://www.portalfiscal.inf.br/nfcom", IsNullable = false)]
+    public class RetNFCom : XMLBase
+    {
+        [XmlAttribute(AttributeName = "versao", DataType = "token")]
+        public string Versao { get; set; }
+
+        [XmlElement("tpAmb")]
+        public TipoAmbiente TpAmb { get; set; }
+
+        [XmlIgnore]
+        public UFBrasil CUF { get; set; }
+
+        [XmlElement("cUF")]
+        public int CUFField
+        {
+            get => (int)CUF;
+            set => CUF = (UFBrasil)Enum.Parse(typeof(UFBrasil), value.ToString());
+        }
+
+        [XmlElement("verAplic")]
+        public string VerAplic { get; set; }
+
+        [XmlElement("cStat")]
+        public int CStat { get; set; }
+
+        [XmlElement("xMotivo")]
+        public string XMotivo { get; set; }
+
+        [XmlElement("protNFCom")]
+        public ProtNFCom ProtNFCom { get; set; }
+    }
+}
