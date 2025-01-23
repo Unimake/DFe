0671bef7ec704e3b34fa59745450c62380d38f03
Wesley Castilho <wesley@unimake.com.br>
2025-01-23 09:05:36 -0300

    test: Cria o teste unitário de serialização/deserialização do retorno da NFCom. ID #169786

--- source/Unimake.DFe.Test/NFCom/SerializacaoDesserializacaoTest.cs
+++ source/Unimake.DFe.Test/NFCom/SerializacaoDesserializacaoTest.cs
@@ -28,6 +28,24 @@ namespace Unimake.DFe.Test.NFCom
 
         [Theory]
         [Trait("DFe", "NFCom")]
+        [InlineData(@"..\..\..\NFCom\Resources\retNFCom.xml")]
+        public void SerializacaoDesserializacaoRetNFCom(string arqXML)
+        {
+            Assert.True(File.Exists(arqXML), "Arquivo " + arqXML + " não foi localizado para a realização da serialização/desserialização.");
+
+            var doc = new XmlDocument();
+            doc.Load(arqXML);
+
+            var nfCom = new Unimake.Business.DFe.Xml.NFCom.RetNFCom();
+            var xml = nfCom.LerXML<Unimake.Business.DFe.Xml.NFCom.RetNFCom>(doc);
+
+            var doc2 = xml.GerarXML();
+
+            Assert.True(doc.InnerText == doc2.InnerText, "XML gerado pela DLL está diferente do conteúdo do arquivo serializado.");
+        }
+
+        [Theory]
+        [Trait("DFe", "NFCom")]
         [InlineData(@"..\..\..\NFCom\Resources\protNFCom.xml")]
         public void SerializacaoDesserializacaoProtNFCom(string arqXML)
         {
