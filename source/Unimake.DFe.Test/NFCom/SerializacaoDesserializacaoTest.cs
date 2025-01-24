676bc8c1804b8e295ff860d33846c02ddba204e2
Shiguemoto <shiguemoto001@gmail.com>
2025-01-23 23:46:11 -0300

    feat: Criação da classe RetEventoNFCom; Criação das classes de serialização e deserialização ProcEventoNFCom da NFCom. ID #169965

--- source/Unimake.DFe.Test/NFCom/SerializacaoDesserializacaoTest.cs
+++ source/Unimake.DFe.Test/NFCom/SerializacaoDesserializacaoTest.cs
@@ -79,5 +79,23 @@ namespace Unimake.DFe.Test.NFCom
 
             Assert.True(doc.InnerText == doc2.InnerText, "XML gerado pela DLL está diferente do conteúdo do arquivo serializado.");
         }
+
+        [Theory]
+        [Trait("DFe", "NFCom")]
+        [InlineData(@"..\..\..\NFCom\Resources\retEventoNFCom.xml")]
+        public void SerializacaoDesserializacaoRetEventoNFCom(string arqXML)
+        {
+            Assert.True(File.Exists(arqXML), "Arquivo " + arqXML + " não foi localizado para a realização da serialização/desserialização.");
+
+            var doc = new XmlDocument();
+            doc.Load(arqXML);
+
+            var nfCom = new Unimake.Business.DFe.Xml.NFCom.RetEventoNFCom();
+            var xml = nfCom.LerXML<Unimake.Business.DFe.Xml.NFCom.RetEventoNFCom>(doc);
+
+            var doc2 = xml.GerarXML();
+
+            Assert.True(doc.InnerText == doc2.InnerText, "XML gerado pela DLL está diferente do conteúdo do arquivo serializado.");
+        }
     }
 }
