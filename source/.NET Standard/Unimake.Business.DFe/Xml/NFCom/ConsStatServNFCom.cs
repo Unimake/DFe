76daa4ffbd1e66c3930e4f5da921a2808de5e0c3
Katllen Sirotenco <katsirotenco@gmail.com>
2025-01-24 09:45:49 -0400

    feat:  Criação das classes consStatServ e retconsStatServ de serialização e deserialização para consulta status serviço da NFCom. ID #169783

---
+++ source/.NET Standard/Unimake.Business.DFe/Xml/NFCom/ConsStatServNFCom.cs
@@ -0,0 +1,30 @@
+﻿#pragma warning disable CS1591
+
+#if INTEROP
+using System.Runtime.InteropServices;
+#endif
+using System;
+using System.Xml.Serialization;
+using Unimake.Business.DFe.Servicos;
+
+namespace Unimake.Business.DFe.Xml.NFCom
+{
+#if INTEROP
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [ProgId("Unimake.Business.DFe.Xml.NFCom.ConsStatServNFCom")]
+    [ComVisible(true)]
+#endif
+    [Serializable()]
+    [XmlRoot("consStatServNFCom", Namespace = "http://www.portalfiscal.inf.br/nfcom", IsNullable = false)]
+    public class ConsStatServNFCom : XMLBase
+    {
+        [XmlElement("tpAmb")]
+        public TipoAmbiente TpAmb { get; set; }
+
+        [XmlElement("xServ")]
+        public string XServ { get; set; }
+
+        [XmlAttribute(AttributeName = "versao")]
+        public string versao { get; set; }
+    }
+}
