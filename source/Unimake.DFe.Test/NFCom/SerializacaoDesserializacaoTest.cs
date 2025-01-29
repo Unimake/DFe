f8e07e054828581cd25c480dc1428d9f7b3fa59a
Shiguemoto <shiguemoto001@gmail.com>
2025-01-29 14:37:25 -0300

    test:  Teste unitário de serialização/deserialização da ConsSitNFCom; Criação das classes de serialização e deserialização para consulta situação da NFCom. ID #170039

--- source/Unimake.DFe.Test/NFCom/SerializacaoDesserializacaoTest.cs
+++ source/Unimake.DFe.Test/NFCom/SerializacaoDesserializacaoTest.cs
@@ -133,5 +133,23 @@ namespace Unimake.DFe.Test.NFCom
 
             Assert.True(doc.InnerText == doc2.InnerText, "XML gerado pela DLL está diferente do conteúdo do arquivo serializado.");
         }
+
+        [Theory]
+        [Trait("DFe", "NFCom")]
+        [InlineData(@"..\..\..\NFCom\Resources\consSitNFCom-ped-sit.xml")]
+        public void SerializacaoDesserializacaoConsSitNFCom(string arqXML)
+        {
+            Assert.True(File.Exists(arqXML), "Arquivo " + arqXML + " não foi localizado para a realização da serialização/desserialização.");
+
+            var doc = new XmlDocument();
+            doc.Load(arqXML);
+
+            var nfCom = new Unimake.Business.DFe.Xml.NFCom.ConsSitNFCom();
+            var xml = nfCom.LerXML<Unimake.Business.DFe.Xml.NFCom.ConsSitNFCom>(doc);
+
+            var doc2 = xml.GerarXML();
+
+            Assert.True(doc.InnerText == doc2.InnerText, "XML gerado pela DLL está diferente do conteúdo do arquivo serializado.");
+        }
     }
 }
