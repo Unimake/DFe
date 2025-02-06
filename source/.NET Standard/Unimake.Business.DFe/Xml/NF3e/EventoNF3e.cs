7c3316b728eabc7ab35a8a2a010381aba8cfe506
Wesley Castilho <wesley@unimake.com.br>
2025-02-06 17:46:34 -0300

    refactor: Refatora a classe de serialização/deserialização do EventoNF3e; apaga a antiga classe do evento de cancelamento da NF3e. ID #170169

--- source/.NET Standard/Unimake.Business.DFe/Xml/NF3e/EventoNF3e.cs
+++ source/.NET Standard/Unimake.Business.DFe/Xml/NF3e/EventoNF3e.cs
@@ -6,6 +6,12 @@ using System.Runtime.InteropServices;
 using System;
 using System.Xml.Serialization;
 using Unimake.Business.DFe.Servicos;
+using System.Xml;
+using System.Reflection;
+using Unimake.Business.DFe.Utility;
+using System.Collections.Generic;
+using System.Xml.Schema;
+using System.Text;
 
 namespace Unimake.Business.DFe.Xml.NF3e
 {
@@ -15,6 +21,7 @@ namespace Unimake.Business.DFe.Xml.NF3e
     [ComVisible(true)]
 #endif
     [Serializable()]
+    [XmlType(Namespace = "http://www.portalfiscal.inf.br/nf3e")]
     [XmlRoot("eventoNF3e", Namespace = "http://www.portalfiscal.inf.br/nf3e", IsNullable = false)]
     public class EventoNF3e : XMLBase
     {
@@ -26,6 +33,30 @@ namespace Unimake.Business.DFe.Xml.NF3e
 
         [XmlElement(ElementName = "Signature", Namespace = "http://www.w3.org/2000/09/xmldsig#")]
         public Signature Signature { get; set; }
+
+        #region Public Methods
+
+        /// <summary>
+        /// Desserializar o XML no objeto EventoNF3e
+        /// </summary>
+        /// <param name="filename">Localização do arquivo XML do EventoNF3e</param>
+        /// <returns>Objeto do EventoNF3e</returns>
+        public EventoNF3e LoadFromFile(string filename)
+        {
+            var doc = new XmlDocument();
+            doc.LoadXml(System.IO.File.ReadAllText(filename, Encoding.UTF8));
+            return XMLUtility.Deserializar<EventoNF3e>(doc);
+        }
+
+        /// <summary>
+        /// Desserializar o XML EventoNF3e no objeto EventoNF3e
+        /// </summary>
+        /// <param name="xml">string do XML EventoNF3e</param>
+        /// <returns>Objeto da EventoNF3e</returns>
+        public EventoNF3e LoadFromXML(string xml) => XMLUtility.Deserializar<EventoNF3e>(xml);
+
+        #endregion Public Methods
+
     }
 
 #if INTEROP
@@ -35,8 +66,18 @@ namespace Unimake.Business.DFe.Xml.NF3e
 #endif
     public class InfEvento
     {
+        #region Private fields
+
+        private EventoDetalhe _detEvento;
+
+        #endregion Private fields
+        
         [XmlAttribute(AttributeName = "Id", DataType = "token")]
-        public string Versao { get; set; }
+        public string Id
+        {
+            get => "ID" + ((int)TpEvento).ToString() + ChNF3e + NSeqEvento.ToString("000");
+            set => _ = value;
+        }
         
         [XmlIgnore]
         public UFBrasil COrgao { get; set; }
@@ -79,23 +120,171 @@ namespace Unimake.Business.DFe.Xml.NF3e
         public TipoEventoNF3e TpEvento { get; set; }
 
         [XmlElement("nSeqEvento")]
-        public string NSeqEvento { get; set; }
+        public int NSeqEvento { get; set; }
 
         [XmlElement("detEvento")]
-        public DetEvento DetEvento {  get; set; }
+        public EventoDetalhe DetEvento
+        {
+            get => _detEvento;
+            set
+            {
+                switch (TpEvento)
+                {
+                    case 0:
+                        _detEvento = value;
+                        break;
+
+                    case TipoEventoNF3e.Cancelamento:
+                        _detEvento = new DetEventoCanc();
+                        break;
+
+                    default:
+                        throw new NotImplementedException($"O tipo de evento '{TpEvento}' não está implementado.");
+                }
+
+                _detEvento.XmlReader = value.XmlReader;
+                _detEvento.ProcessReader();
+            }
+        }
+
+        public InfEvento() { }
+
+        public InfEvento(EventoDetalhe detEvento) => DetEvento = detEvento ?? throw new ArgumentNullException(nameof(detEvento));
     }
 
 #if INTEROP
     [ClassInterface(ClassInterfaceType.AutoDual)]
-    [ProgId("Unimake.Business.DFe.Xml.NF3e.DetEvento")]
+    [ProgId("Unimake.Business.DFe.Xml.NF3e.EventoDetalhe")]
     [ComVisible(true)]
 #endif
-    public class DetEvento
+    public class EventoDetalhe : IXmlSerializable
+    {
+        #region Internal Properties
+
+        internal XmlReader XmlReader { get; set; }
+
+        private static readonly BindingFlags bindingFlags = BindingFlags.Public |
+            BindingFlags.Instance |
+            BindingFlags.IgnoreCase;
+
+        private static readonly List<string> hasField = new List<string>
+        {
+            "COrgaoAutor",
+        };
+
+        #endregion Internal Properties
+
+        #region Internal Methods
+
+        internal virtual void ProcessReader()
+        {
+            if (XmlReader == null)
+            {
+                return;
+            }
+
+            var type = GetType();
+
+            if (XmlReader.HasAttributes)
+            {
+                if (XmlReader.GetAttribute("versaoEvento") != "")
+                {
+                    var pi = type.GetProperty("versaoEvento", BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase);
+                    pi?.SetValue(this, XmlReader.GetAttribute("versaoEvento"));
+                }
+            }
+
+            while (XmlReader.Read())
+            {
+                if (XmlReader.NodeType != XmlNodeType.Element)
+                {
+                    continue;
+                }
+
+                SetValue(type);
+            }
+        }
+
+        internal virtual void SetValue(Type type)
+        {
+            var pi = GetPropertyInfo(type);
+
+            if (pi == null)
+            {
+                return;
+            }
+
+            SetValue(pi);
+        }
+
+        internal virtual void SetValue(PropertyInfo pi) =>
+            pi?.SetValue(this, Converter.ToAny(XmlReader.GetValue<object>(XmlReader.Name), pi.PropertyType));
+
+        protected internal PropertyInfo GetPropertyInfo(Type type)
+        {
+            var pi = hasField.Exists(w => w.ToLower() == XmlReader.Name.ToLower()) ? 
+                type.GetProperty(XmlReader.Name + "Field", bindingFlags) : type.GetProperty(XmlReader.Name, bindingFlags);
+
+            return pi;
+        }
+
+        #endregion Internal Methods
+
+        #region Public Properties
+
+        [XmlElement("descEvento")]
+        public virtual string DescEvento { get; set; }
+
+        [XmlAttribute(AttributeName = "versaoEvento", DataType = "token")]
+        public virtual string VersaoEvento { get; set; }
+
+        #endregion Public Properties
+
+        #region Public Methods
+
+        public XmlSchema GetSchema() => default;
+
+        public void ReadXml(XmlReader reader) => XmlReader = reader;
+
+        public virtual void WriteXml(XmlWriter writer) => writer.WriteAttributeString("versaoEvento", VersaoEvento);
+
+        #endregion Public Methods
+
+    }
+
+#if INTEROP
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [ProgId("Unimake.Business.DFe.Xml.NF3e.DetEventoCanc")]
+    [ComVisible(true)]
+#endif
+    public class DetEventoCanc : EventoDetalhe
     {
         [XmlAttribute("versaoEvento", DataType = "string")]
-        public string VersaoEvento { get; set; }
+        public override string VersaoEvento { get; set; }
 
-        [XmlElement("evCancNF3e", Namespace = "http://www.portalfiscal.inf.br/nf3e")]
-        public EvCancNF3e EvCancNF3e { get; set; }
+        [XmlElement("descEvento")]
+        public override string DescEvento { get; set; } = "Cancelamento";
+
+        [XmlElement("nProt")]
+        public string NProt { get; set; }
+
+        [XmlElement("xJust")]
+        public string XJust { get; set; }
+
+        #region Public Methods
+
+        public override void WriteXml(XmlWriter writer)
+        {
+            base.WriteXml(writer);
+
+            writer.WriteRaw($@"
+            <evCancNF3e>
+            <descEvento>{DescEvento}</descEvento>
+            <nProt>{NProt}</nProt>
+            <xJust>{XJust}</xJust>
+            </evCancNF3e>");
+        }
+
+        #endregion Public Methods
     }
 }
