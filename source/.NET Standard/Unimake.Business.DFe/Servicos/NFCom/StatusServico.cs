0c64172f68be58f6640128542410d72d887d7877
Katllen Sirotenco <katsirotenco@gmail.com>
2025-02-11 15:15:10 -0400

    feat: Criar serviço de consultar status serviço da NFCom. ID #170165

---
+++ source/.NET Standard/Unimake.Business.DFe/Servicos/NFCom/StatusServico.cs
@@ -0,0 +1,165 @@
+﻿#if INTEROP
+using System.Runtime.InteropServices;
+#endif
+using System;
+using System.Xml;
+using Unimake.Business.DFe.Servicos.Interop;
+using Unimake.Business.DFe.Utility;
+using Unimake.Business.DFe.Xml.NFCom;
+using Unimake.Exceptions;
+
+namespace Unimake.Business.DFe.Servicos.NFCom
+{
+    /// <summary>
+    /// Enviar o XML de consulta status do serviço da NFCom para o web-service
+    /// </summary>
+#if INTEROP
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [ProgId("Unimake.Business.DFe.Servicos.NFCom.StatusServico")]
+    [ComVisible(true)]
+#endif
+    public class StatusServico : ServicoBase, IInteropService<ConsStatServNFCom>
+    {
+        #region Protected Methods
+
+        /// <summary>
+        /// Definir o valor de algumas das propriedades do objeto "Configuracoes"
+        /// </summary>
+        protected override void DefinirConfiguracao()
+        {
+            var xml = new ConsStatServNFCom();
+            xml = xml.LerXML<ConsStatServNFCom>(ConteudoXML);
+
+            if (!Configuracoes.Definida)
+            {
+                Configuracoes.Servico = Servico.NFComStatusServico;
+                Configuracoes.TipoAmbiente = xml.TpAmb;
+                Configuracoes.SchemaVersao = xml.Versao;
+
+                base.DefinirConfiguracao();
+            }
+        }
+
+        #endregion Protected Method
+
+        #region Public Properties
+
+        /// <summary>
+        /// Conteúdo retornado pelo web-service depois do envio do XML
+        /// </summary>
+        public RetConsStatServNFCom Result
+        {
+            get
+            {
+                if (!string.IsNullOrWhiteSpace(RetornoWSString))
+                {
+                    return XMLUtility.Deserializar<RetConsStatServNFCom>(RetornoWSXML);
+                }
+
+                return new RetConsStatServNFCom
+                {
+                    CStat = 0,
+                    XMotivo = "Ocorreu uma falha ao tentar criar o objeto a partir do XML retornado da SEFAZ."
+                };
+            }
+        }
+
+        #endregion Public Properties
+
+        #region Public Constructors
+
+        /// <summary>
+        /// Construtor
+        /// </summary>
+        public StatusServico() : base() { }
+
+        /// <summary>
+        /// Construtor
+        /// </summary>
+        /// <param name="consStatServNFCom">Objeto contendo o XML a ser enviado</param>
+        /// <param name="configuracao">Configurações para conexão e envio do XML para o web-service</param>
+        public StatusServico(ConsStatServNFCom consStatServNFCom, Configuracao configuracao) : this()
+        {
+            if (configuracao is null)
+            {
+                throw new ArgumentNullException(nameof(configuracao));
+            }
+
+            Inicializar(consStatServNFCom?.GerarXML() ?? throw new ArgumentNullException(nameof(consStatServNFCom)), configuracao);
+        }
+
+        /// <summary>
+        /// Construtor
+        /// </summary>
+        /// <param name="conteudoXML">String do XML a ser enviado</param>
+        /// <param name="configuracao">Configurações para conexão e envio do XML para o web-service</param>
+        public StatusServico(string conteudoXML, Configuracao configuracao) : this()
+        {
+            if (configuracao is null)
+            {
+                throw new ArgumentNullException(nameof(configuracao));
+            }
+
+            var doc = new XmlDocument();
+            doc.LoadXml(conteudoXML);
+
+            Inicializar(doc, configuracao);
+        }
+
+        #endregion Public Constructors
+
+        #region Public Methods
+
+#if INTEROP
+
+        /// <summary>
+        /// Executa o serviço: Assina o XML, valida e envia para o web-service
+        /// </summary>
+        /// <param name="consStatServNFCom">Objeto contendo o XML a ser enviado</param>
+        /// <param name="configuracao">Configurações a serem utilizadas na conexão e envio do XML para o web-service</param>
+        [ComVisible(true)]
+        public void Executar([MarshalAs(UnmanagedType.IUnknown)] ConsStatServNFCom consStatServNFCom, [MarshalAs(UnmanagedType.IUnknown)] Configuracao configuracao)
+        {
+            try
+            {
+                if (configuracao is null)
+                {
+                    throw new ArgumentNullException(nameof(configuracao));
+                }
+
+                Inicializar(consStatServNFCom?.GerarXML() ?? throw new ArgumentNullException(nameof(consStatServNFCom)), configuracao);
+                Executar();
+            }
+            catch (ValidarXMLException ex)
+            {
+                Exceptions.ThrowHelper.Instance.Throw(ex);
+            }
+            catch (CertificadoDigitalException ex)
+            {
+                ThrowHelper.Instance.Throw(ex);
+            }
+            catch (Exception ex)
+            {
+                ThrowHelper.Instance.Throw(ex);
+            }
+        }
+
+#endif
+
+        /// <inheritdoc />
+        public override void GravarXmlDistribuicao(string pasta, string nomeArquivo, string conteudoXML)
+        {
+            try
+            {
+                throw new Exception("Não existe XML de distribuição para consulta status do serviço.");
+            }
+            catch (Exception ex)
+            {
+                ThrowHelper.Instance.Throw(ex);
+            }
+        }
+
+        #endregion Public Methods
+    }
+}
+    
