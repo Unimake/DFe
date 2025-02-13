2c4905d101c0b1413cfff36d4a7f163198018c04
Katllen Sirotenco <katsirotenco@gmail.com>
2025-02-13 14:35:34 -0400

    test: Criação do teste unitário para consultar status do serviço de NFCom. ID #170165

---
+++ source/Unimake.DFe.Test/NFCom/StatusServicoTest.cs
@@ -0,0 +1,125 @@
+﻿using Unimake.Business.DFe.Servicos;
+using Unimake.Business.DFe.Servicos.NFCom;
+using Unimake.Business.DFe.Xml.NFCom;
+using Xunit;
+
+namespace Unimake.DFe.Test.NFCom
+{
+    /// <summary>
+    /// Testar o serviço de consulta protocolo da NFCom
+    /// </summary>
+    public class StatusServicoTest
+    {
+        /// <summary>
+        /// Consultar o status do serviço da NFCom somente para saber se a conexão com o webservice está ocorrendo corretamente e se quem está respondendo é o webservice correto.
+        /// Efetua uma consulta por estado + ambiente para garantir que todos estão funcionando.
+        /// </summary>
+        /// <param name="ufBrasil">UF para onde deve ser enviado a consulta status</param>
+        /// <param name="tipoAmbiente">Ambiente para onde deve ser enviado a consulta status</param>
+        [Theory]
+        [Trait("DFe", "NFCom")]
+        [InlineData(UFBrasil.AC, TipoAmbiente.Homologacao)]
+        [InlineData(UFBrasil.AL, TipoAmbiente.Homologacao)]
+        [InlineData(UFBrasil.AP, TipoAmbiente.Homologacao)]
+        [InlineData(UFBrasil.AM, TipoAmbiente.Homologacao)]
+        [InlineData(UFBrasil.BA, TipoAmbiente.Homologacao)]
+        [InlineData(UFBrasil.CE, TipoAmbiente.Homologacao)]
+        [InlineData(UFBrasil.DF, TipoAmbiente.Homologacao)]
+        [InlineData(UFBrasil.ES, TipoAmbiente.Homologacao)]
+        [InlineData(UFBrasil.GO, TipoAmbiente.Homologacao)]
+        [InlineData(UFBrasil.MA, TipoAmbiente.Homologacao)]
+        [InlineData(UFBrasil.PA, TipoAmbiente.Homologacao)]
+        [InlineData(UFBrasil.PB, TipoAmbiente.Homologacao)]
+        [InlineData(UFBrasil.PR, TipoAmbiente.Homologacao)]
+        [InlineData(UFBrasil.PE, TipoAmbiente.Homologacao)]
+        [InlineData(UFBrasil.PI, TipoAmbiente.Homologacao)]
+        [InlineData(UFBrasil.RJ, TipoAmbiente.Homologacao)]
+        [InlineData(UFBrasil.RN, TipoAmbiente.Homologacao)]
+        [InlineData(UFBrasil.RS, TipoAmbiente.Homologacao)]
+        [InlineData(UFBrasil.RO, TipoAmbiente.Homologacao)]
+        [InlineData(UFBrasil.RR, TipoAmbiente.Homologacao)]
+        [InlineData(UFBrasil.SC, TipoAmbiente.Homologacao)]
+        [InlineData(UFBrasil.SP, TipoAmbiente.Homologacao)]
+        [InlineData(UFBrasil.SE, TipoAmbiente.Homologacao)]
+        [InlineData(UFBrasil.TO, TipoAmbiente.Homologacao)]
+        [InlineData(UFBrasil.AC, TipoAmbiente.Producao)]
+        [InlineData(UFBrasil.AL, TipoAmbiente.Producao)]
+        [InlineData(UFBrasil.AP, TipoAmbiente.Producao)]
+        [InlineData(UFBrasil.AM, TipoAmbiente.Producao)]
+        [InlineData(UFBrasil.BA, TipoAmbiente.Producao)]
+        [InlineData(UFBrasil.CE, TipoAmbiente.Producao)]
+        [InlineData(UFBrasil.DF, TipoAmbiente.Producao)]
+        [InlineData(UFBrasil.ES, TipoAmbiente.Producao)]
+        [InlineData(UFBrasil.GO, TipoAmbiente.Producao)]
+        [InlineData(UFBrasil.MA, TipoAmbiente.Producao)]
+        [InlineData(UFBrasil.PA, TipoAmbiente.Producao)]
+        [InlineData(UFBrasil.PB, TipoAmbiente.Producao)]
+        [InlineData(UFBrasil.PR, TipoAmbiente.Producao)]
+        [InlineData(UFBrasil.PE, TipoAmbiente.Producao)]
+        [InlineData(UFBrasil.PI, TipoAmbiente.Producao)]
+        [InlineData(UFBrasil.RJ, TipoAmbiente.Producao)]
+        [InlineData(UFBrasil.RN, TipoAmbiente.Producao)]
+        [InlineData(UFBrasil.RS, TipoAmbiente.Producao)]
+        [InlineData(UFBrasil.RO, TipoAmbiente.Producao)]
+        [InlineData(UFBrasil.RR, TipoAmbiente.Producao)]
+        [InlineData(UFBrasil.SC, TipoAmbiente.Producao)]
+        [InlineData(UFBrasil.SP, TipoAmbiente.Producao)]
+        [InlineData(UFBrasil.SE, TipoAmbiente.Producao)]
+        [InlineData(UFBrasil.TO, TipoAmbiente.Producao)]
+        public void ConsultarStatusServico(UFBrasil ufBrasil, TipoAmbiente tipoAmbiente)
+        {
+            var xml = new ConsStatServNFCom
+            {
+                Versao = "1.00",
+                XServ = "STATUS",
+                TpAmb = tipoAmbiente
+            };
+
+            var configuracao = new Configuracao
+            {
+                TipoDFe = TipoDFe.NFCom,
+                TipoEmissao = TipoEmissao.Normal,
+                CodigoUF = (int)ufBrasil,
+                CertificadoDigital = PropConfig.CertificadoDigital
+            };
+
+            var statusServico = new StatusServico(xml, configuracao);
+            statusServico.Executar();
+
+            Assert.True(configuracao.CodigoUF.Equals((int)ufBrasil), "UF definida nas configurações diferente de " + ufBrasil.ToString());
+            Assert.True(configuracao.TipoAmbiente.Equals(tipoAmbiente), "Tipo de ambiente definido nas configurações diferente de " + tipoAmbiente.ToString());
+            Assert.True(statusServico.Result.TpAmb.Equals(tipoAmbiente), "Webservice retornou um Tipo de ambiente diferente " + tipoAmbiente.ToString());
+            Assert.True(statusServico.Result.CStat.Equals(107) || statusServico.Result.CStat.Equals(656), "Serviço não está em operação - <xMotivo>" + statusServico.Result.XMotivo + "<xMotivo>");
+        }
+
+        [Theory]
+        [Trait("DFe", "NFCom")]
+        [InlineData(UFBrasil.PR, TipoAmbiente.Producao)]
+        public void ConsultarStatusServicoString(UFBrasil ufBrasil, TipoAmbiente tipoAmbiente)
+        {
+            var xml = new ConsStatServNFCom
+            {
+                Versao = "1.00",
+                XServ = "STATUS",
+                TpAmb = tipoAmbiente
+            };
+
+            var configuracao = new Configuracao
+            {
+                TipoDFe = TipoDFe.NFCom,
+                TipoEmissao = TipoEmissao.Normal,
+                CodigoUF = (int)ufBrasil,
+                CertificadoDigital = PropConfig.CertificadoDigital
+            };
+
+            var statusServico = new StatusServico(xml.GerarXML().OuterXml, configuracao);
+            statusServico.Executar();
+
+            Assert.True(configuracao.CodigoUF.Equals((int)ufBrasil), "UF definida nas configurações diferente de " + ufBrasil.ToString());
+            Assert.True(configuracao.TipoAmbiente.Equals(tipoAmbiente), "Tipo de ambiente definido nas configurações diferente de " + tipoAmbiente.ToString());
+            Assert.True(statusServico.Result.TpAmb.Equals(tipoAmbiente), "Webservice retornou um Tipo de ambiente diferente " + tipoAmbiente.ToString());
+            Assert.True(statusServico.Result.CStat.Equals(107) || statusServico.Result.CStat.Equals(656), "Serviço não está em operação - <xMotivo>" + statusServico.Result.XMotivo + "<xMotivo>");
+        }
+
+    }
+}
