ffe3c7ddcb7e86e56f954bb8338172fbef3537af
Shiguemoto <shiguemoto001@gmail.com>
2025-02-27 18:58:53 -0300

    docs: Classe DistDFeInt; Criar documentação nas classes da CTe. ID #171074

--- source/.NET Standard/Unimake.Business.DFe/Xml/CTe/DistDFeInt.cs
+++ source/.NET Standard/Unimake.Business.DFe/Xml/CTe/DistDFeInt.cs
@@ -9,6 +9,9 @@ using Unimake.Business.DFe.Servicos;
 
 namespace Unimake.Business.DFe.Xml.CTe
 {
+    /// <summary>
+    /// Distribuição de DF-e de interesse
+    /// </summary>
 #if INTEROP
     [ClassInterface(ClassInterfaceType.AutoDual)]
     [ProgId("Unimake.Business.DFe.Xml.CTe.DistDFeInt")]
@@ -17,15 +20,27 @@ namespace Unimake.Business.DFe.Xml.CTe
     [XmlRoot("distDFeInt", Namespace = "http://www.portalfiscal.inf.br/cte", IsNullable = false)]
     public class DistDFeInt : XMLBase
     {
+        /// <summary>
+        /// Versão do leiaute.
+        /// </summary>
         [XmlAttribute(AttributeName = "versao", DataType = "token")]
         public string Versao { get; set; }
 
+        /// <summary>
+        /// Tipo do Ambiente.
+        /// </summary>
         [XmlElement("tpAmb")]
         public TipoAmbiente TpAmb { get; set; }
 
+        /// <summary>
+        /// Código da UF do Autor.
+        /// </summary>
         [XmlIgnore]
         public UFBrasil CUFAutor { get; set; }
 
+        /// <summary>
+        /// Código da UF do Autor (campo para serialização XML).
+        /// </summary>
         [XmlElement("cUFAutor")]
         public int CUFAutorField
         {
@@ -33,27 +48,49 @@ namespace Unimake.Business.DFe.Xml.CTe
             set => CUFAutor = (UFBrasil)Enum.Parse(typeof(UFBrasil), value.ToString());
         }
 
+        /// <summary>
+        /// Código do órgão.
+        /// </summary>
         [XmlIgnore]
         public readonly UFBrasil COrgao = UFBrasil.AN;
 
+        /// <summary>
+        /// CNPJ do interessado.
+        /// </summary>
         [XmlElement("CNPJ")]
         public string CNPJ { get; set; }
 
+        /// <summary>
+        /// CPF do interessado.
+        /// </summary>
         [XmlElement("CPF")]
         public string CPF { get; set; }
 
+        /// <summary>
+        /// Consulta por NSU.
+        /// </summary>
         [XmlElement("consNSU")]
         public ConsNSU ConsNSU { get; set; }
 
+        /// <summary>
+        /// Consulta por NSU inicial.
+        /// </summary>
         [XmlElement("distNSU")]
         public DistNSU DistNSU { get; set; }
 
         #region ShouldSerialize
 
+        /// <summary>
+        /// Verifica se a propriedade CNPJ deve ser serializada.
+        /// </summary>
         public bool ShouldSerializeCNPJ()
         {
             return !string.IsNullOrWhiteSpace(CNPJ);
         }
+
+        /// <summary>
+        /// Verifica se a propriedade CPF deve ser serializada.
+        /// </summary>
         public bool ShouldSerializeCPF()
         {
             return !string.IsNullOrWhiteSpace(CPF);
@@ -70,6 +107,9 @@ namespace Unimake.Business.DFe.Xml.CTe
     [XmlType(AnonymousType = true, Namespace = "http://www.portalfiscal.inf.br/cte")]
     public class ConsNSU
     {
+        /// <summary>
+        /// NSU da consulta.
+        /// </summary>
         [XmlElement("NSU", DataType = "token")]
         public string NSU { get; set; }
     }
@@ -82,6 +122,9 @@ namespace Unimake.Business.DFe.Xml.CTe
     [XmlType(AnonymousType = true, Namespace = "http://www.portalfiscal.inf.br/cte")]
     public class DistNSU
     {
+        /// <summary>
+        /// Último NSU da consulta.
+        /// </summary>
         [XmlElement("ultNSU", DataType = "token")]
         public string UltNSU { get; set; }
     }
