aec0c183543bea7883fefc5062fd21e602557b33
Shiguemoto <shiguemoto001@gmail.com>
2025-03-06 20:02:50 -0300

    docs: Classe ConsReciMDFe; Criar documentação nas classes da MDFe. ID #171200

--- source/.NET Standard/Unimake.Business.DFe/Xml/MDFe/ConsReciMDFe.cs
+++ source/.NET Standard/Unimake.Business.DFe/Xml/MDFe/ConsReciMDFe.cs
@@ -8,20 +8,32 @@ using Unimake.Business.DFe.Servicos;
 
 namespace Unimake.Business.DFe.Xml.MDFe
 {
+    /// <summary>
+    /// Consulta Recibo MDFe
+    /// </summary>
 #if INTEROP
     [ClassInterface(ClassInterfaceType.AutoDual)]
     [ProgId("Unimake.Business.DFe.Xml.MDFe.ConsReciMDFe")]
     [ComVisible(true)]
 #endif
     [XmlRoot("consReciMDFe", Namespace = "http://www.portalfiscal.inf.br/mdfe", IsNullable = false)]
-    public class ConsReciMDFe: XMLBase
+    public class ConsReciMDFe : XMLBase
     {
+        /// <summary>
+        /// Versão do leiaute
+        /// </summary>
         [XmlAttribute(AttributeName = "versao", DataType = "token")]
         public string Versao { get; set; }
 
+        /// <summary>
+        /// Tipo de Ambiente
+        /// </summary>
         [XmlElement("tpAmb")]
         public TipoAmbiente TpAmb { get; set; }
 
+        /// <summary>
+        /// Número do Recibo
+        /// </summary>
         [XmlElement("nRec")]
         public string NRec { get; set; }
     }
