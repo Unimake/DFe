6656977a3f7017a09ae02767244476310c7f9940
Shiguemoto <shiguemoto001@gmail.com>
2025-03-06 20:05:02 -0300

    docs: Classe RetConsReciMDFe; Criar documentação nas classes da MDFe. ID #171200

--- source/.NET Standard/Unimake.Business.DFe/Xml/MDFe/RetConsReciMDFe.cs
+++ source/.NET Standard/Unimake.Business.DFe/Xml/MDFe/RetConsReciMDFe.cs
@@ -19,27 +19,51 @@ namespace Unimake.Business.DFe.Xml.MDFe
     [XmlRoot("retConsReciMDFe", Namespace = "http://www.portalfiscal.inf.br/mdfe", IsNullable = false)]
     public class RetConsReciMDFe : XMLBase
     {
+        /// <summary>
+        /// Versão do leiaute.
+        /// </summary>
         [XmlAttribute(AttributeName = "versao", DataType = "token")]
         public string Versao { get; set; }
 
+        /// <summary>
+        /// Tipo de ambiente.
+        /// </summary>
         [XmlElement("tpAmb")]
         public TipoAmbiente TpAmb { get; set; }
 
+        /// <summary>
+        /// Versão do aplicativo que processou o retorno da consulta do recibo do MDFe.
+        /// </summary>
         [XmlElement("verAplic")]
         public string VerAplic { get; set; }
 
+        /// <summary>
+        /// Número do recibo.
+        /// </summary>
         [XmlElement("nRec")]
         public string NRec { get; set; }
 
+        /// <summary>
+        /// Código do status da resposta.
+        /// </summary>
         [XmlElement("cStat")]
         public int CStat { get; set; }
 
+        /// <summary>
+        /// Descrição literal do status da resposta.
+        /// </summary>
         [XmlElement("xMotivo")]
         public string XMotivo { get; set; }
 
+        /// <summary>
+        /// Código da UF.
+        /// </summary>
         [XmlIgnore]
         public UFBrasil CUF { get; set; }
 
+        /// <summary>
+        /// Propriedade auxiliar para serialização/desserialização do XML (Utilize sempre a propriedade "CUF" para atribuir ou resgatar o valor)
+        /// </summary>
         [XmlElement("cUF")]
         public int CUFField
         {
@@ -47,6 +71,9 @@ namespace Unimake.Business.DFe.Xml.MDFe
             set => CUF = (UFBrasil)Enum.Parse(typeof(UFBrasil), value.ToString());
         }
 
+        /// <summary>
+        /// Data e hora do recebimento.
+        /// </summary>
         [XmlIgnore]
 #if INTEROP
         public DateTime DhRecbto { get; set; }
@@ -54,6 +81,9 @@ namespace Unimake.Business.DFe.Xml.MDFe
         public DateTimeOffset DhRecbto { get; set; }
 #endif
 
+        /// <summary>
+        /// Propriedade auxiliar para serialização/desserialização do XML (Utilize sempre a propriedade "DhRecbto" para atribuir ou resgatar o valor)
+        /// </summary>
         [XmlElement("dhRecbto")]
         public string DhRecbtoField
         {
@@ -71,13 +101,16 @@ namespace Unimake.Business.DFe.Xml.MDFe
             }
         }
 
+        /// <summary>
+        /// Protocolo de autorização do MDFe.
+        /// </summary>
         [XmlElement("protMDFe")]
-        public List<ProtMDFe> ProtMDFe { get; set; }
+        public List<ProtMDFe> ProtMDFe { get; set; } = new List<ProtMDFe>();
 
 #if INTEROP
 
         /// <summary>
-        /// Retorna o elemento da lista ProtMDFe (Utilizado para linguagens diferentes do CSharp que não conseguem pegar o conteúdo da lista ProtMDFe)
+        /// Retorna o elemento da lista ProtMDFe (Utilizado para linguagens diferentes do CSharp que não conseguem pegar o conteúdo da lista ProtMDFe).
         /// </summary>
         /// <param name="index">Índice da lista a ser retornado (Começa com 0 (zero))</param>
         /// <returns>Conteúdo do index passado por parâmetro da ProtMDFe</returns>
@@ -92,7 +125,7 @@ namespace Unimake.Business.DFe.Xml.MDFe
         }
 
         /// <summary>
-        /// Retorna a quantidade de elementos existentes na lista ProtMDFe
+        /// Retorna a quantidade de elementos existentes na lista ProtMDFe.
         /// </summary>
         public int GetProtMDFeCount => (ProtMDFe != null ? ProtMDFe.Count : 0);
 
