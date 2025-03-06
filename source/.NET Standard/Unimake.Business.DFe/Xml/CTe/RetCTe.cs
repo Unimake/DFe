4f36a7abdc1dd13eb38ea5e0ecb6b61e3a723a14
Shiguemoto <shiguemoto001@gmail.com>
2025-03-06 17:16:11 -0300

    docs: Classe RetCTe; Criar documentação nas classes da CTe. ID #171199

--- source/.NET Standard/Unimake.Business.DFe/Xml/CTe/RetCTe.cs
+++ source/.NET Standard/Unimake.Business.DFe/Xml/CTe/RetCTe.cs
@@ -17,17 +17,29 @@ namespace Unimake.Business.DFe.Xml.CTe
     [Serializable()]
     [XmlType(Namespace = "http://www.portalfiscal.inf.br/cte")]
     [XmlRoot("retCTe", Namespace = "http://www.portalfiscal.inf.br/cte", IsNullable = false)]
-    public class RetCTe: XMLBase
+    public class RetCTe : XMLBase
     {
+        /// <summary>
+        /// Versão do leiaute XML
+        /// </summary>
         [XmlAttribute(AttributeName = "versao", DataType = "token")]
         public string Versao { get; set; }
 
+        /// <summary>
+        /// Tipo do Ambiente
+        /// </summary>
         [XmlElement("tpAmb")]
         public TipoAmbiente TpAmb { get; set; }
 
+        /// <summary>
+        /// Código da UF que atendeu a solicitação.
+        /// </summary>
         [XmlIgnore]
         public UFBrasil CUF { get; set; }
 
+        /// <summary>
+        /// Propriedade auxiliar para serialização/desserialização do XML (Utilize sempre a propriedade "CUF" para atribuir ou resgatar o valor)
+        /// </summary>
         [XmlElement("cUF")]
         public int CUFField
         {
@@ -35,15 +47,27 @@ namespace Unimake.Business.DFe.Xml.CTe
             set => CUF = (UFBrasil)Enum.Parse(typeof(UFBrasil), value.ToString());
         }
 
+        /// <summary>
+        /// Versão do Aplicativo
+        /// </summary>
         [XmlElement("verAplic")]
         public string VerAplic { get; set; }
 
+        /// <summary>
+        /// Código do status da resposta
+        /// </summary>
         [XmlElement("cStat")]
         public int CStat { get; set; }
 
+        /// <summary>
+        /// Descrição literal do status da resposta
+        /// </summary>
         [XmlElement("xMotivo")]
         public string XMotivo { get; set; }
 
+        /// <summary>
+        /// Protocolo do CT-e
+        /// </summary>
         [XmlElement("protCTe")]
         public ProtCTe ProtCTe { get; set; }
     }
