d0fc42b0cb9434d598e086fdd2ee3df523e71122
Wesley Castilho <wesley@unimake.com.br>
2025-07-18 11:56:46 -0300

    test: Cria um teste unitário para validação do exemplo de NFSe de padrão NACIONAL. ID #

---
+++ source/Unimake.DFe.Test/NFSe/ValidarXmlNfseNacionalTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Unimake.Business.DFe;
+using Unimake.Business.DFe.Servicos;
+using Xunit;
+
+namespace Unimake.DFe.Test.NFSe
+{
+    public class ValidarXmlNfseNacionalTest
+    {
+        [Theory]
+        [Trait("NFSe", "ValidarXmlNfseNacional")]
+        [InlineData(@"..\..\..\NFSe\Resources\NACIONAL\1.00\GerarNfseEnvio-env-loterps.xml", "NFSe.NACIONAL.DPS_v1.00.xsd")]
+        [InlineData(@"..\..\..\NFSe\Resources\NACIONAL\1.00\GerarNfseEnvio_RTC-env-loterps.xml", "NFSe.NACIONAL.DPS_v1.00.xsd")]
+        public void ValidarXmlNfseNacional(string arqXML, string arqXSD)
+        {
+            Assert.True(File.Exists(arqXML), "Arquivo " + arqXML + " não foi localizado para a realização do teste de obter o tipo do XML.");
+
+            var doc = new XmlDocument();
+            doc.Load(arqXML);
+
+            var validar = new ValidarSchema();
+            validar.Validar(doc, arqXSD, "http://www.sped.fazenda.gov.br/nfse", PadraoNFSe.NACIONAL);
+
+            Assert.True(validar.Success, "Ocorreu um erro na validação de SCHEMA: \n" + validar.ErrorMessage);
+        }
+    }
+}
