71c948f37a6e147fd9c10625c48e05a43e9566ee
Wesley Castilho <wesley@unimake.com.br>
2025-08-05 11:22:43 -0300

    test: Cria teste unitário do validador de IBSCBS do NFCom. ID #175322

---
+++ source/Unimake.DFe.Test/NFCom/NFComValidatorTest.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Xml;
+using Unimake.Business.DFe.Validator;
+using Xunit;
+
+namespace Unimake.DFe.Test.NFCom
+{
+    /// <summary>
+    /// Classe de teste unitário para testar o validator da NFCom
+    /// </summary>
+    public class NFComValidatorTest
+    {
+        /// <summary>
+        /// Testa o XML da NFCom com tags da RTC com CSTs 000, 200 e 410
+        /// </summary>
+        /// <param name="arqXML">Caminho do arquivo XML que será testado</param>
+        [Theory]
+        [Trait("DFe", "NFCom")]
+        [InlineData(@"..\..\..\NFCom\Resources\RTC\NFCom_CST000_CST200_CST410.xml")]
+        public void ValidarNFComComIBSCBSPeloValidator(string arqXML)
+        {
+            Assert.True(File.Exists(arqXML), "Arquivo " + arqXML + " não foi localizado para a realização da validação.");
+
+            var doc = new XmlDocument();
+            doc.Load(arqXML);
+
+            Assert.True(ValidatorFactory.BuidValidator(doc.InnerXml)?.Validate());
+        }
+    }
+}
