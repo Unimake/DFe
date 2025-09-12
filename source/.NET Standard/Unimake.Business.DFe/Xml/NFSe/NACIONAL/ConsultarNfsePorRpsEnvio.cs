648c83406b4be2d887811c10d26df1b525fd88c0
Eduardo Cavazin <eduardotorcav@gmail.com>
2025-09-12 09:45:43 -0300

    feat: Adiciona classe de serialização e deserialização para o serviço de Consultar NFS-e por RPS do padrão NACIONAL de NFSe. ID #176588

--- source/.NET Standard/Unimake.Business.DFe/Xml/NFSe/NACIONAL/ConsultarNfsePorRpsEnvio.cs
+++ source/.NET Standard/Unimake.Business.DFe/Xml/NFSe/NACIONAL/ConsultarNfsePorRpsEnvio.cs
@@ -7,18 +7,17 @@ using System.Xml.Serialization;
 
 namespace Unimake.Business.DFe.Xml.NFSe.NACIONAL
 {
-
     /// <summary>
-    /// Classe da consulta da NFS-e de padrão NACIONAL;
+    /// Classe da consulta da NFS-e por RPS de padrão NACIONAL;
     /// </summary>
 #if INTEROP
     [ClassInterface(ClassInterfaceType.AutoDual)]
-    [ProgId("Unimake.Business.DFe.Xml.NFSe.Nacional.ConsultarNfseEnvio")]
+    [ProgId("Unimake.Business.DFe.Xml.NFSe.Nacional.ConsultarNfsePorRpsEnvio")]
     [ComVisible(true)]
-#endif
+#endif 
     [Serializable]
-    [XmlRoot("NFSe", Namespace = "http://www.sped.fazenda.gov.br/nfse", IsNullable = false)]
-    public class ConsultarNfseEnvio : XMLBase
+    [XmlRoot("DPS", Namespace = "http://www.sped.fazenda.gov.br/nfse", IsNullable = false)]
+    public class ConsultarNfsePorRpsEnvio : XMLBase
     {
         /// <summary>
         /// Versão do schema do XML de consulta da NFS-e
@@ -29,21 +28,21 @@ namespace Unimake.Business.DFe.Xml.NFSe.NACIONAL
         /// <summary>
         /// Informações da NFS-e a ser consultada
         /// </summary>
-        [XmlElement("infNFSe")]
-        public InfNFSeConsulta InfNFSe {  get; set; } = new InfNFSeConsulta();
+        [XmlElement("infDPS")]
+        public InfDPSConsulta InfDPS { get; set; } = new InfDPSConsulta();
     }
 
     /// <summary>
     /// Informações da NFS-e a ser consultada
     /// </summary>
     [Serializable]
-    public class InfNFSeConsulta
+    public class InfDPSConsulta
     {
-
         /// <summary>
         /// ID da NFS-e a ser consultada
         /// </summary>
         [XmlAttribute("Id")]
         public string Id { get; set; }
     }
+
 }
