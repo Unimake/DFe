1918a3a48864948fd05fd7c5b48c0235c00af11d
Eduardo Cavazin <eduardotorcav@gmail.com>
2025-09-17 16:15:09 -0300

    refactor: Ajustes de padronização da classe de serialização ConsultarNfsePorRps do padrão NACIONAL de NFSe. ID #176640

--- source/.NET Standard/Unimake.Business.DFe/Xml/NFSe/NACIONAL/ConsultarNfsePorRpsEnvio.cs
+++ source/.NET Standard/Unimake.Business.DFe/Xml/NFSe/NACIONAL/ConsultarNfsePorRpsEnvio.cs
@@ -1,48 +1,56 @@
-﻿#if INTEROP
+﻿#pragma warning disable CS1591
+
+#if INTEROP
 using System.Runtime.InteropServices;
 #endif
+
 using System;
-using System.Xml;
 using System.Xml.Serialization;
 
 namespace Unimake.Business.DFe.Xml.NFSe.NACIONAL
 {
     /// <summary>
-    /// Classe da consulta da NFS-e por RPS de padrão NACIONAL;
+    /// Consulta de NFS-e por RPS (Padrão Nacional – DPS).
     /// </summary>
 #if INTEROP
     [ClassInterface(ClassInterfaceType.AutoDual)]
-    [ProgId("Unimake.Business.DFe.Xml.NFSe.Nacional.ConsultarNfsePorRpsEnvio")]
+    [ProgId("Unimake.Business.DFe.Xml.NFSe.NACIONAL.ConsultarNfsePorRpsEnvio")]
     [ComVisible(true)]
-#endif 
+#endif
     [Serializable]
+    [XmlType(Namespace = "http://www.sped.fazenda.gov.br/nfse")]
     [XmlRoot("DPS", Namespace = "http://www.sped.fazenda.gov.br/nfse", IsNullable = false)]
     public class ConsultarNfsePorRpsEnvio : XMLBase
     {
         /// <summary>
-        /// Versão do schema do XML de consulta da NFS-e
+        /// Versão do schema do XML de consulta por RPS (DPS).
         /// </summary>
-        [XmlAttribute(AttributeName = "versao")]
+        [XmlAttribute(AttributeName = "versao", DataType = "token")]
         public string Versao { get; set; }
 
         /// <summary>
-        /// Informações da NFS-e a ser consultada
+        /// Informações do DPS a ser consultado.
         /// </summary>
         [XmlElement("infDPS")]
         public InfDPSConsulta InfDPS { get; set; } = new InfDPSConsulta();
     }
 
     /// <summary>
-    /// Informações da NFS-e a ser consultada
+    /// Informações do DPS a ser consultado.
     /// </summary>
+#if INTEROP
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [ProgId("Unimake.Business.DFe.Xml.NFSe.NACIONAL.InfDPSConsulta")]
+    [ComVisible(true)]
+#endif
     [Serializable]
+    [XmlType(Namespace = "http://www.sped.fazenda.gov.br/nfse")]
     public class InfDPSConsulta
     {
         /// <summary>
-        /// ID da NFS-e a ser consultada
+        /// Id do DPS (fornecido externamente; não é calculado aqui).
         /// </summary>
-        [XmlAttribute("Id")]
+        [XmlAttribute("Id", DataType = "token")]
         public string Id { get; set; }
     }
-
 }
