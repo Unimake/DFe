c2d1e74e0c835816128a3adb06857d56a2908b57
Eduardo Cavazin <eduardotorcav@gmail.com>
2025-09-17 16:32:36 -0300

    checkpoint: Ajuste de nomeclatura de serialiação e deserialização. ID #176695

--- source/.NET Standard/Unimake.Business.DFe/Xml/NFSe/NACIONAL/CancelarNfse.cs
+++ source/.NET Standard/Unimake.Business.DFe/Xml/NFSe/NACIONAL/CancelarNfse.cs
@@ -1,54 +1,50 @@
-﻿#if INTEROP
+﻿#pragma warning disable CS1591
+#if INTEROP
 using System.Runtime.InteropServices;
 #endif
 using System;
-using System.Xml;
 using System.Xml.Serialization;
 using Unimake.Business.DFe.Servicos;
-using System.Xml.Schema;
 
 namespace Unimake.Business.DFe.Xml.NFSe.NACIONAL
 {
-
     internal static class NfseNs
     {
         public const string Ns = "http://www.sped.fazenda.gov.br/nfse";
     }
 
-    ///<summary>
-    ///Pedido de Cancelamento de NFS-e padrão NACIONAL
-    ///</summary>
+    /// <summary>
+    /// Pedido de Cancelamento de NFS-e (Padrão Nacional).
+    /// </summary>
 #if INTEROP
     [ClassInterface(ClassInterfaceType.AutoDual)]
     [ProgId("Unimake.Business.DFe.Xml.NFSe.NACIONAL.CancelarNfse")]
     [ComVisible(true)]
 #endif
     [Serializable]
+    [XmlType(Namespace = NfseNs.Ns)]
     [XmlRoot("pedRegEvento", Namespace = NfseNs.Ns, IsNullable = false)]
-    public class CancelarNfseEnvio : XMLBase
+    public class CancelarNfse : XMLBase
     {
         /// <summary>
-        /// Versão do schema do XML de Cancelamento da NFS-e
+        /// Versão do schema do XML de Cancelamento da NFS-e.
         /// </summary>
         [XmlAttribute(AttributeName = "versao", DataType = "token")]
         public string Versao { get; set; }
 
         /// <summary>
-        /// Informações do Pedido de Cancelamento da NFS-e
+        /// Informações do Pedido de Cancelamento da NFS-e.
         /// </summary>
         [XmlElement("infPedReg", Namespace = NfseNs.Ns)]
         public InfPedRegCancelamento InfPedReg { get; set; } = new InfPedRegCancelamento();
 
         /// <summary>
-        /// Assinatura do XML
+        /// Assinatura XML-DSig (se exigida pelo provedor).
         /// </summary>
         [XmlElement("Signature", Namespace = "http://www.w3.org/2000/09/xmldsig#")]
         public Signature Signature { get; set; }
     }
 
-    /// <summary>
-    /// Bloco das informações do Pedido de Cancelamento da NFS-e
-    /// </summary>
 #if INTEROP
     [ClassInterface(ClassInterfaceType.AutoDual)]
     [ProgId("Unimake.Business.DFe.Xml.NFSe.NACIONAL.InfPedRegCancelamento")]
@@ -59,35 +55,33 @@ namespace Unimake.Business.DFe.Xml.NFSe.NACIONAL
     public class InfPedRegCancelamento
     {
         /// <summary>
-        /// Identificador do Pedido de Cancelamento da NFS-e
+        /// Identificador do Pedido de Cancelamento da NFS-e.
         /// </summary>
-        [XmlAttribute(AttributeName = "Id")]
+        [XmlAttribute(AttributeName = "Id", DataType = "token")]
         public string Id { get; set; }
 
         /// <summary>
-        /// Tipo de Ambiente - Produção ou Homologação
+        /// Tipo de Ambiente - Produção ou Homologação.
         /// </summary>
         [XmlElement("tpAmb", Namespace = NfseNs.Ns)]
         public TipoAmbiente TpAmb { get; set; }
 
         /// <summary>
-        /// Versão da Aplicação
+        /// Versão da Aplicação.
         /// </summary>
         [XmlElement("verAplic", Namespace = NfseNs.Ns)]
         public string VerAplic { get; set; }
 
         /// <summary>
-        /// Data e Hora do Evento, formato UTC (AAAA-MM-DDThh:mm:ssTZD, onde TZD = +hh:mm ou -hh:mm)
+        /// Data e hora do evento.
         /// </summary>
         [XmlIgnore]
 #if INTEROP
         public DateTime DhEvento { get; set; }
 #else
-        public DateTimeOffset DhEvento { get; set; }  
+        public DateTimeOffset DhEvento { get; set; }
 #endif
-        /// <summary>
-        /// Propriedade auxiliar para serialização/desserialização do XML (Utilize sempre a propriedade DhEvento para atribuir ou resgatar o valor)
-        /// </summary>
+
         [XmlElement("dhEvento", Namespace = NfseNs.Ns)]
         public string DhEventoField
         {
@@ -97,77 +91,65 @@ namespace Unimake.Business.DFe.Xml.NFSe.NACIONAL
 #else
             set => DhEvento = DateTimeOffset.Parse(value);
 #endif
-
         }
 
         /// <summary>
-        /// CNPJ do Autor do Evento
+        /// CNPJ do autor do evento (opcional se houver CPF).
         /// </summary>
         [XmlElement("CNPJAutor", Namespace = NfseNs.Ns)]
         public string CNPJAutor { get; set; }
 
         /// <summary>
-        /// CPF do Autor do Evento
+        /// CPF do autor do evento (opcional se houver CNPJ).
         /// </summary>
         [XmlElement("CPFAutor", Namespace = NfseNs.Ns)]
         public string CPFAutor { get; set; }
 
         /// <summary>
-        /// Chave da NFS-e a ser cancelada
+        /// Chave da NFS-e a ser cancelada.
         /// </summary>
         [XmlElement("chNFSe", Namespace = NfseNs.Ns)]
         public string ChNFSe { get; set; }
 
         /// <summary>
-        /// Código do Cancelamento
+        /// Número do pedido/registro do evento.
         /// </summary>
         [XmlElement("nPedRegEvento", Namespace = NfseNs.Ns)]
         public string NPedRegEvento { get; set; }
 
         /// <summary>
-        /// Evento de Cancelamento da NFS-e
+        /// Evento de Cancelamento (código 101101).
         /// </summary>
         [XmlElement("e101101", Namespace = NfseNs.Ns)]
-        public EvCanc101101 E101101 { get; set; } = new EvCanc101101();
+        public E101101 E101101 { get; set; } = new E101101();
 
         #region ShouldSerialize
-        /// <summary>
-        /// Método que indica se o campo CNPJAutor deve ser serializado
-        /// </summary>
-        /// <returns></returns>
         public bool ShouldSerializeCNPJAutor() => !string.IsNullOrWhiteSpace(CNPJAutor);
-
-        /// <summary>
-        /// Método que indica se o campo CPFAutor deve ser serializado
-        /// </summary>
-        /// <returns></returns>
         public bool ShouldSerializeCPFAutor() => !string.IsNullOrWhiteSpace(CPFAutor);
+        #endregion
 
-        #endregion ShouldSerialize
     }
 
     /// <summary>
-    /// Bloco do evento de Cancelamento da NFS-e
-    /// </summary>
+    /// Bloco do evento de Cancelamento da NFS-e (e101101).</summary>
+#if INTEROP
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [ProgId("Unimake.Business.DFe.Xml.NFSe.NACIONAL.E101101")]
+    [ComVisible(true)]
+#endif
     [Serializable]
     [XmlType("e101101", Namespace = NfseNs.Ns)]
-    public class EvCanc101101
+    public class E101101
     {
-        /// <summary>
-        /// Descrição do evento.
-        /// </summary>
+        /// <summary>Descrição do evento.</summary>
         [XmlElement("xDesc", Namespace = NfseNs.Ns)]
         public string XDesc { get; set; }
 
-        /// <summary>
-        /// Código do motivo do cancelamento.
-        /// </summary>
+        /// <summary>Código do motivo do cancelamento.</summary>
         [XmlElement("cMotivo", Namespace = NfseNs.Ns)]
         public string CMotivo { get; set; }
 
-        /// <summary>
-        /// Descrição do motivo do cancelamento.
-        /// </summary>
+        /// <summary>Descrição do motivo do cancelamento.</summary>
         [XmlElement("xMotivo", Namespace = NfseNs.Ns)]
         public string XMotivo { get; set; }
     }
