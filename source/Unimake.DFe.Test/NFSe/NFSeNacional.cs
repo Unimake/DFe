7362dc50cfbe5ac7a079286afff28866c2fae7db
Wandrey Mundin <47141566+wandreymundin@users.noreply.github.com>
2025-09-26 10:24:28 -0300

    feat: Atualização do pacote de schemas da NFSe nacional e ajustado DLL para validar contra schema para este padrão. ID #177027

---
+++ source/Unimake.DFe.Test/NFSe/NFSeNacional.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System.Xml;
+using Unimake.Business.DFe.Servicos;
+using Unimake.Business.DFe.Servicos.NFSe;
+using Unimake.Business.DFe.Xml.NFSe.NACIONAL;
+using Xunit;
+
+namespace Unimake.DFe.Test.NFSe.NACIONAL
+{
+    public class ValidacaoSchemaTests
+    {
+        [Theory]
+        [Trait("DFe", "NFSe")]
+        [Trait("Layout", "Nacional")]
+        [InlineData(@"..\..\..\NFSe\Resources\NACIONAL\1.00\2025_09_26-env-loterps.xml")]
+        public void ValidarDPS(string xmlDPS)
+        {
+            Assert.True(File.Exists(xmlDPS), $"Arquivo {xmlDPS} não encontrado.");
+
+            var xmlDoc = new XmlDocument();
+            xmlDoc.Load(xmlDPS);
+
+            var configuracao = new Configuracao
+            {
+                TipoDFe = TipoDFe.NFSe,
+                CertificadoDigital = PropConfig.CertificadoDigital,
+                TipoAmbiente = TipoAmbiente.Producao,
+                CodigoMunicipio = 1001058,
+                Servico = Servico.NFSeGerarNfse,
+                SchemaVersao = "1.00"
+            };
+
+            var gerarNFSe = new GerarNfse(xmlDoc.OuterXml, configuracao);
+            //gerarNFSe.Executar();
+        }
+    }
+}
