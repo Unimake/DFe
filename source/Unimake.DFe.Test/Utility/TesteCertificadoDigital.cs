4d14185536549ca103bcc7a144469b163d6a74dc
Gustavo Randi <gustarandiiii@gmail.com>
2025-10-13 15:25:24 -0300

    test: criação de teste para comparar velocidade dos metodos de carregamento do certificado digital. ID #177557

---
+++ source/Unimake.DFe.Test/Utility/TesteCertificadoDigital.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+using Unimake.Business.DFe.Servicos;
+using Unimake.Business.Security;
+using Xunit;
+using Xunit.Abstractions;
+
+public class TesteCertificadoDigital
+{
+    private readonly ITestOutputHelper _output;
+
+    public TesteCertificadoDigital(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+
+    [Fact]
+    public void TesteCertificadoVelocidade()
+    {
+        var CertificadoCaminho = @"C:\Projetos\Unimake_PV.pfx";
+        var CertificadoSenha = "12345678";
+
+        // Método original
+        var sw1 = Stopwatch.StartNew();
+        for (int i = 0; i < 50; i++)
+        {
+            var certificado = new CertificadoDigital()
+                .CarregarCertificadoDigitalA1(CertificadoCaminho, CertificadoSenha);
+        }
+        sw1.Stop();
+        _output.WriteLine($"Tempo do método original: {sw1.ElapsedMilliseconds} ms");
+
+
+        // Método com Configuracao 
+        var configuracao = new Configuracao
+        {
+            CertificadoArquivo = CertificadoCaminho,
+            CertificadoSenha = CertificadoSenha,
+            TipoDFe = TipoDFe.NFSe
+        };
+
+        var sw2 = Stopwatch.StartNew();
+        for (int i = 0; i < 50; i++)
+        {
+            var certRapido = configuracao.CertificadoDigital;
+        }
+        sw2.Stop();
+        _output.WriteLine($"Tempo do método com Configuração única: {sw2.ElapsedMilliseconds} ms");
+    }
+}
