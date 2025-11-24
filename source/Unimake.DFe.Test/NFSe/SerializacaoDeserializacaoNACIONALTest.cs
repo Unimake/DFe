a89ffe6fa61e171e42391db4783c442f3b5fa734
Eduardo Cavazin <eduardotorcav@gmail.com>
2025-11-24 15:50:41 -0300

    test: Adiciona testes de serialização e desserialização para a versão 1.01 de DPS do padrão NACIONAL de NFSe. ID #178652

--- source/Unimake.DFe.Test/NFSe/SerializacaoDeserializacaoNACIONALTest.cs
+++ source/Unimake.DFe.Test/NFSe/SerializacaoDeserializacaoNACIONALTest.cs
@@ -1,5 +1,7 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Xml;
+using Unimake.Business.DFe.Servicos;
 using Unimake.Business.DFe.Xml.NFSe.NACIONAL;
 using Xunit;
 
@@ -184,6 +186,243 @@ namespace Unimake.DFe.Test.NFSe.NACIONAL
             var nsmgr = new XmlNamespaceManager(doc.NameTable);
             nsmgr.AddNamespace("ns", "http://www.sped.fazenda.gov.br/nfse");
             return nsmgr;
+        }
+
+        [Theory]
+        [Trait("DFe", "NFSe")]
+        [Trait("Layout", "Nacional")]
+        [Trait("Versao", "1.01")]
+        [InlineData(@"..\..\..\NFSe\Resources\NACIONAL\1.01\GerarNfseEnvio-env-loterps.xml")]
+        public void GerarNfseNACIONAL_V101_ComIBSCBS_CasoReal(string caminhoXml)
+        {
+            Assert.True(File.Exists(caminhoXml), $"Arquivo {caminhoXml} não encontrado.");
+
+            var docFixture = new XmlDocument();
+            docFixture.Load(caminhoXml);
+
+            // Deserialização
+            var lido = new DPS().LerXML<DPS>(docFixture);
+
+            // Verificações básicas v1.01
+            Assert.Equal("1.01", lido.Versao);
+            Assert.NotNull(lido.infDPS);
+            Assert.Equal("DPS410690211234567800019500001000000000000001", lido.infDPS.Id);
+
+            // Verificações específicas do IBSCBS v1.01
+            var ibscbs = lido.infDPS.IBSCBS;
+            Assert.NotNull(ibscbs);
+
+            // Novos campos obrigatórios v1.01
+            Assert.Equal(0, ibscbs.IndDest); // Novo campo obrigatório
+
+            // Novos campos opcionais v1.01 (presentes neste XML)
+            Assert.NotNull(ibscbs.TpOper);
+            Assert.Equal(TpOperacaoGov.FornecicoComPagamentoPosterior, ibscbs.TpOper); // tpOper=1
+
+            // Campos existentes
+            Assert.Equal(0, ibscbs.FinNFSe);
+            Assert.Equal(0, ibscbs.IndFinal);
+            Assert.Equal(1, ibscbs.CIndOp); // cIndOp="000001"
+            Assert.Equal(TipoEnteGovernamental.Uniao, ibscbs.TpEnteGov); // tpEnteGov=1
+
+            // Campo opcional não presente neste XML
+            Assert.Null(ibscbs.GRefNFSe); // Não informado neste caso
+
+            // Verificação da estrutura de valores IBSCBS
+            Assert.NotNull(ibscbs.Valores);
+            Assert.NotNull(ibscbs.Valores.Trib);
+            Assert.NotNull(ibscbs.Valores.Trib.GIBSCBS);
+
+            var gIBSCBS = ibscbs.Valores.Trib.GIBSCBS;
+            Assert.Equal(101, gIBSCBS.CST);
+            Assert.Equal(1, gIBSCBS.CClassTrib); // "000001"
+            Assert.Equal(1, gIBSCBS.CCredPres); // "01"
+
+            // Serialização (round-trip)
+            var docRoundTrip = lido.GerarXML();
+            Assert.NotNull(docRoundTrip);
+
+            // Verificação da estrutura XML resultante
+            var nsmgr = CreateNamespaceManager(docRoundTrip);
+            var ibscbsNode = docRoundTrip.SelectSingleNode("//ns:IBSCBS", nsmgr);
+            Assert.NotNull(ibscbsNode);
+
+            // Verifica presença dos campos v1.01 corretos
+            Assert.NotNull(ibscbsNode.SelectSingleNode("ns:indDest", nsmgr));
+            Assert.NotNull(ibscbsNode.SelectSingleNode("ns:tpOper", nsmgr));
+            Assert.NotNull(ibscbsNode.SelectSingleNode("ns:tpEnteGov", nsmgr));
+
+            // Verifica ausência dos campos v1.00 removidos
+            Assert.Null(ibscbsNode.SelectSingleNode("ns:indPessoas", nsmgr));
+            Assert.Null(ibscbsNode.SelectSingleNode("ns:adq", nsmgr));
+
+            // Verifica campo opcional não informado
+            Assert.Null(ibscbsNode.SelectSingleNode("ns:gRefNFSe", nsmgr));
+
+            // Teste de criação do zero com mesmos valores
+            var criado = new DPS
+            {
+                Versao = "1.01",
+                infDPS = new infDPS
+                {
+                    Id = lido.infDPS.Id,
+                    TpAmb = lido.infDPS.TpAmb,
+                    DhEmi = lido.infDPS.DhEmi,
+                    VerAplic = lido.infDPS.VerAplic,
+                    Serie = lido.infDPS.Serie,
+                    NDPS = lido.infDPS.NDPS,
+                    DCompet = lido.infDPS.DCompet,
+                    TpEmit = lido.infDPS.TpEmit,
+                    CLocEmi = lido.infDPS.CLocEmi,
+                    Prest = lido.infDPS.Prest,
+                    Toma = lido.infDPS.Toma,
+                    Serv = lido.infDPS.Serv,
+                    Valores = new Valores
+                    {
+                        VServPrest = lido.infDPS.Valores.VServPrest,
+                        Trib = lido.infDPS.Valores.Trib
+                    },
+                    IBSCBS = new IBSCBS
+                    {
+                        FinNFSe = ibscbs.FinNFSe,
+                        IndFinal = ibscbs.IndFinal,
+                        CIndOp = ibscbs.CIndOp,
+                        TpOper = ibscbs.TpOper, // v1.01
+                        TpEnteGov = ibscbs.TpEnteGov,
+                        IndDest = ibscbs.IndDest, // v1.01
+                        Valores = ibscbs.Valores
+                    }
+                }
+            };
+
+            var docCriado = criado.GerarXML();
+
+            // Compara elementos essenciais
+            var ibscbsCriado = docCriado.SelectSingleNode("//ns:IBSCBS", nsmgr);
+            Assert.NotNull(ibscbsCriado);
+
+            // Verifica valores específicos
+            Assert.Equal("0", ibscbsCriado.SelectSingleNode("ns:indDest", nsmgr)?.InnerText);
+            Assert.Equal("1", ibscbsCriado.SelectSingleNode("ns:tpOper", nsmgr)?.InnerText);
+            Assert.Equal("1", ibscbsCriado.SelectSingleNode("ns:tpEnteGov", nsmgr)?.InnerText);
+        }
+
+        [Fact]
+        [Trait("DFe", "NFSe")]
+        [Trait("Layout", "Nacional")]
+        [Trait("Versao", "1.01")]
+        public void IBSCBS_V101_ShouldSerialize_CenarioReal()
+        {
+         
+            var ibscbs = new IBSCBS
+            {
+                FinNFSe = 0,
+                IndFinal = 0,
+                CIndOp = 1,
+                TpOper = TpOperacaoGov.FornecicoComPagamentoPosterior,
+                TpEnteGov = TipoEnteGovernamental.Municipio, 
+                IndDest = 0, 
+            };
+
+            Assert.True(ibscbs.ShouldSerializeTpOper());
+            Assert.True(ibscbs.ShouldSerializeTpEnteGov());
+
+            Assert.False(ibscbs.ShouldSerializeXTpEnteGov());
+
+            var dps = new DPS
+            {
+                Versao = "1.01",
+                infDPS = new infDPS
+                {
+                    Id = "DPS_TESTE_V101",
+                    TpAmb = TipoAmbiente.Homologacao,
+                    DhEmi = DateTime.Now, 
+                    VerAplic = "1.01.01",            
+                    Serie = 1,                        
+                    NDPS = 1,                         
+                    DCompet = DateTime.Now,    
+                    TpEmit = TipoEmitenteNFSe.Prestador, 
+                    CLocEmi = 4202909,                
+
+                    // Dados mínimos obrigatórios
+                    Prest = new Prest
+                    {
+                        CNPJ = "99999999999999",
+                        RegTrib = new RegTrib
+                        {
+                            OpSimpNac = OptSimplesNacional.NaoOptante,
+                            RegEspTrib = RegEspTrib.Nenhum
+                        }
+                    },
+
+                    Toma = new Toma
+                    {
+                        CNPJ = "00000000000000",
+                        XNome = "TESTE LTDA"
+                    },
+
+                    Serv = new Serv
+                    {
+                        LocPrest = new LocPrest
+                        {
+                            CLocPrestacao = 4202909
+                        },
+                        CServ = new CServ
+                        {
+                            CTribNac = "140101",
+                            XDescServ = "Teste de servico v1.01"
+                        }
+                    },
+
+                    Valores = new Valores
+                    {
+                        VServPrest = new VServPrest
+                        {
+                            VServ = 10.00
+                        }
+                    },
+                    IBSCBS = ibscbs 
+                }
+            };
+
+            // Agora a serialização deve funcionar
+            var xml = dps.GerarXML();
+            var nsmgr = CreateNamespaceManager(xml);
+            var node = xml.SelectSingleNode("//ns:IBSCBS", nsmgr);
+
+            Assert.NotNull(node);
+            Assert.NotNull(node.SelectSingleNode("ns:tpOper", nsmgr));
+            Assert.NotNull(node.SelectSingleNode("ns:indDest", nsmgr));
+            Assert.Null(node.SelectSingleNode("ns:gRefNFSe", nsmgr));
+
+            // Verifica valores corretos
+            Assert.Equal("1", node.SelectSingleNode("ns:tpOper", nsmgr)?.InnerText);
+            Assert.Equal("0", node.SelectSingleNode("ns:indDest", nsmgr)?.InnerText);
+        }
+
+        [Fact]
+        [Trait("DFe", "NFSe")]
+        [Trait("Layout", "Nacional")]
+        [Trait("Versao", "1.01")]
+        public void IBSCBS_V101_ShouldSerialize_Isolado()
+        {
+            // Teste isolado apenas do ShouldSerialize (sem serialização XML completa)
+            var ibscbs = new IBSCBS();
+
+            // Testa ShouldSerialize para campos opcionais v1.01 (valores padrão)
+            Assert.False(ibscbs.ShouldSerializeTpOper()); // null
+            Assert.False(ibscbs.ShouldSerializeTpEnteGov()); // null
+            Assert.False(ibscbs.ShouldSerializeXTpEnteGov()); // null/empty
+
+            // Define valores
+            ibscbs.TpOper = TpOperacaoGov.FornecicoComPagamentoPosterior;
+            ibscbs.TpEnteGov = TipoEnteGovernamental.Municipio;
+            ibscbs.XTpEnteGov = "Descrição";
+
+            // Testa ShouldSerialize com valores informados
+            Assert.True(ibscbs.ShouldSerializeTpOper());
+            Assert.True(ibscbs.ShouldSerializeTpEnteGov());
+            Assert.True(ibscbs.ShouldSerializeXTpEnteGov());
         }
 
     }
