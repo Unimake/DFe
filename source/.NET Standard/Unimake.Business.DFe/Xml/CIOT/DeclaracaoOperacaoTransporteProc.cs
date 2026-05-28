345dc0dc1870dcb6dc80b14306ccd4c04a37c896
Wandrey Mundin <47141566+wandreymundin@users.noreply.github.com>
2026-05-28 16:05:08 -0300

    feat: Ajustes diversos nas classes de serialização do CIOT. ID #184179

---
+++ source/.NET Standard/Unimake.Business.DFe/Xml/CIOT/DeclaracaoOperacaoTransporteProc.cs
@@ -0,0 +1,58 @@
+﻿#pragma warning disable CS1591
+
+#if INTEROP
+using System.Runtime.InteropServices;
+#endif
+
+using System;
+using System.Reflection;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+using Unimake.Business.DFe.Utility;
+
+namespace Unimake.Business.DFe.Xml.CIOT
+{
+#if INTEROP
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [ProgId("Unimake.Business.DFe.Xml.CIOT.DeclaracaoOperacaoTransporteProc")]
+    [ComVisible(true)]
+#endif
+    [Serializable()]
+    [XmlType(Namespace = CIOTNamespace.PortalANTT)]
+    [XmlRoot("DeclaracaoOperacaoTransporteProc", Namespace = CIOTNamespace.PortalANTT, IsNullable = false)]
+
+    public class DeclaracaoOperacaoTransporteProc : XMLBase
+    {
+        [XmlAttribute(AttributeName = "versao", DataType = "token")]
+        public string Versao { get; set; }
+
+        [XmlElement("DeclaracaoOperacaoTransporte")]
+        public DeclaracaoOperacaoTransporte DeclaracaoOperacaoTransporte { get; set; }
+
+        [XmlElement("RetDeclaracaoOperacaoTransporte")]
+        public RetDeclaracaoOperacaoTransporte RetDeclaracaoOperacaoTransporte { get; set; }
+
+        [XmlIgnore]
+        public string NomeArquivoDistribuicao => RetDeclaracaoOperacaoTransporte.CodigoIdentificacaoOperacao + "-procCIOT.xml";
+
+        public override XmlDocument GerarXML()
+        {
+            var xmlDocument = base.GerarXML();
+            var attribute = GetType().GetCustomAttribute<XmlRootAttribute>();
+            ((XmlElement)xmlDocument.GetElementsByTagName("DeclaracaoOperacaoTransporte")[0]).SetAttribute("xmlns", attribute.Namespace);
+            ((XmlElement)xmlDocument.GetElementsByTagName("RetDeclaracaoOperacaoTransporte")[0]).SetAttribute("xmlns", attribute.Namespace);
+
+            return xmlDocument;
+        }
+
+        public DeclaracaoOperacaoTransporteProc LoadFromFile(string filename)
+        {
+            var doc = new XmlDocument();
+            doc.LoadXml(System.IO.File.ReadAllText(filename, Encoding.UTF8));
+            return XMLUtility.Deserializar<DeclaracaoOperacaoTransporteProc>(doc);
+        }
+
+        public DeclaracaoOperacaoTransporteProc LoadFromXML(string xml) => XMLUtility.Deserializar<DeclaracaoOperacaoTransporteProc>(xml);
+    }
+}
