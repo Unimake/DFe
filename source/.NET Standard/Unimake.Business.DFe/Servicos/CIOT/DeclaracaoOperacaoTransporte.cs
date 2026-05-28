9aee547884aadc7b738c456276086d4199c72a07
Wandrey Mundin <47141566+wandreymundin@users.noreply.github.com>
2026-05-28 16:06:13 -0300

    feat: Criado métodos para gravar o XML de Distribuição. ID #184179

--- source/.NET Standard/Unimake.Business.DFe/Servicos/CIOT/DeclaracaoOperacaoTransporte.cs
+++ source/.NET Standard/Unimake.Business.DFe/Servicos/CIOT/DeclaracaoOperacaoTransporte.cs
@@ -1,8 +1,10 @@
 #if INTEROP
 using System.Runtime.InteropServices;
 #endif
+using System;
 using Unimake.Business.DFe.Servicos.Interop;
 using Unimake.Business.DFe.Xml.CIOT;
+using Unimake.Exceptions;
 
 namespace Unimake.Business.DFe.Servicos.CIOT
 {
@@ -16,6 +18,73 @@ namespace Unimake.Business.DFe.Servicos.CIOT
 #endif
     public class DeclaracaoOperacaoTransporte : ServicoBase<Xml.CIOT.DeclaracaoOperacaoTransporte, RetDeclaracaoOperacaoTransporte>, IInteropService<Xml.CIOT.DeclaracaoOperacaoTransporte>
     {
+        #region Private Fields
+
+        private readonly System.Collections.Generic.Dictionary<string, DeclaracaoOperacaoTransporteProc> DeclaracaoOperacaoTransporteProcs = new System.Collections.Generic.Dictionary<string, DeclaracaoOperacaoTransporteProc>();
+
+        #endregion Private Fields
+
+        #region Public Properties
+
+        /// <summary>
+        /// Propriedade contendo o XML da declaração de operação de transporte com o retorno da API anexado para geração do arquivo de distribuição.
+        /// A chave do dicionário é o código de identificação da operação retornado no campo <c>CodigoIdentificacaoOperacao</c>.
+        /// </summary>
+        public System.Collections.Generic.Dictionary<string, DeclaracaoOperacaoTransporteProc> DeclaracaoOperacaoTransporteProcResults
+        {
+            get
+            {
+                if (Result?.Temp != null)
+                {
+                    throw new Exception($"Não é possível gerar o XML de distribuição porque o serviço retornou erro: {Result.Temp.Error} - {Result.Temp.Message}");
+                }
+
+                if (string.IsNullOrWhiteSpace(Result?.CodigoIdentificacaoOperacao))
+                {
+                    throw new Exception("Não foi localizado o CódigoIdentificacaoOperacao no retorno do serviço para elaboração do arquivo de distribuição.");
+                }
+
+                var codigoIdentificacaoOperacao = Result.CodigoIdentificacaoOperacao;
+
+                if (DeclaracaoOperacaoTransporteProcs.ContainsKey(codigoIdentificacaoOperacao))
+                {
+                    DeclaracaoOperacaoTransporteProcs[codigoIdentificacaoOperacao].RetDeclaracaoOperacaoTransporte = Result;
+                }
+                else
+                {
+                    DeclaracaoOperacaoTransporteProcs.Add(codigoIdentificacaoOperacao, new DeclaracaoOperacaoTransporteProc
+                    {
+                        Versao = Configuracoes.SchemaVersao,
+                        DeclaracaoOperacaoTransporte = Envio,
+                        RetDeclaracaoOperacaoTransporte = Result
+                    });
+                }
+
+                return DeclaracaoOperacaoTransporteProcs;
+            }
+        }
+
+        /// <summary>
+        /// Recupera o conteúdo processado da declaração de operação de transporte, combinando o XML de envio com o XML de retorno para geração do arquivo de distribuição.
+        /// </summary>
+        public DeclaracaoOperacaoTransporteProc DeclaracaoOperacaoTransporteProcResult
+        {
+            get
+            {
+                var codigoIdentificacaoOperacao = Result?.CodigoIdentificacaoOperacao;
+
+                if (string.IsNullOrWhiteSpace(codigoIdentificacaoOperacao))
+                {
+                    _ = DeclaracaoOperacaoTransporteProcResults;
+                    codigoIdentificacaoOperacao = Result.CodigoIdentificacaoOperacao;
+                }
+
+                return DeclaracaoOperacaoTransporteProcResults[codigoIdentificacaoOperacao];
+            }
+        }
+
+        #endregion Public Properties
+
         /// <inheritdoc />
         protected override Servico ServicoCIOT => Servico.CIOTDeclaracaoOperacaoTransporte;
 
@@ -42,7 +111,7 @@ namespace Unimake.Business.DFe.Servicos.CIOT
 
 #if INTEROP
         /// <summary>
-        /// Executa o serviço
+        /// Executa o serviço de declaração de operação de transporte via COM, inicializando os dados de envio e configurações antes de transmitir a requisição para a API.
         /// </summary>
         [ComVisible(true)]
         public void Executar([MarshalAs(UnmanagedType.IUnknown)] Xml.CIOT.DeclaracaoOperacaoTransporte xml, [MarshalAs(UnmanagedType.IUnknown)] Configuracao configuracao)
@@ -50,6 +119,79 @@ namespace Unimake.Business.DFe.Servicos.CIOT
             InicializarServico(xml, configuracao);
             Executar();
         }
+
+        /// <summary>
+        /// Recupera o XML processado da declaração de operação de transporte no formato string para consumo por linguagens que acessam a biblioteca via COM/Interop.
+        /// </summary>
+        /// <returns>XML de distribuição no formato DeclaracaoOperacaoTransporteProc.</returns>
+        [ComVisible(true)]
+        public string GetDeclaracaoOperacaoTransporteProcResult() => DeclaracaoOperacaoTransporteProcResult.GerarXML().OuterXml;
+
+        /// <summary>
+        /// Recupera o XML de distribuição da declaração de operação de transporte no formato string a partir do código de identificação da operação, permitindo acesso por linguagens que utilizam COM/Interop.
+        /// </summary>
+        /// <param name="codigoIdentificacaoOperacao">Código de identificação da operação de transporte para retornar o XML de distribuição correspondente.</param>
+        /// <returns>XML de distribuição da declaração no formato DeclaracaoOperacaoTransporteProc.</returns>
+        [ComVisible(true)]
+        public string GetDeclaracaoOperacaoTransporteProcResults(string codigoIdentificacaoOperacao)
+        {
+            var retornar = "";
+
+            if (DeclaracaoOperacaoTransporteProcResults.Count > 0)
+            {
+                retornar = DeclaracaoOperacaoTransporteProcResults[codigoIdentificacaoOperacao].GerarXML().OuterXml;
+            }
+
+            return retornar;
+        }
 #endif
+
+        #region Public Methods
+
+        /// <summary>
+        /// Grava o XML de distribuição da declaração de operação de transporte em uma pasta do disco, gerando automaticamente o nome do arquivo com base no conteúdo retornado pela API.
+        /// </summary>
+        /// <param name="pasta">Pasta onde o XML de distribuição será gravado.</param>
+        public void GravarXmlDistribuicao(string pasta)
+        {
+            try
+            {
+                var declaracaoOperacaoTransporteProc = DeclaracaoOperacaoTransporteProcResult;
+                GravarXmlDistribuicao(pasta, declaracaoOperacaoTransporteProc.NomeArquivoDistribuicao, declaracaoOperacaoTransporteProc.GerarXML().OuterXml);
+            }
+            catch (Exception ex)
+            {
+                ThrowHelper.Instance.Throw(ex);
+            }
+        }
+
+        /// <summary>
+        /// Grava o XML de distribuição da declaração de operação de transporte em um stream informado pelo consumidor, permitindo manipulação em memória ou persistência customizada.
+        /// </summary>
+        /// <param name="stream">Stream de destino que receberá o conteúdo do XML de distribuição.</param>
+#if INTEROP
+        [ComVisible(false)]
+#endif
+        public void GravarXmlDistribuicao(System.IO.Stream stream)
+        {
+            try
+            {
+                if (stream is null)
+                {
+                    throw new ArgumentNullException(nameof(stream));
+                }
+
+                var value = DeclaracaoOperacaoTransporteProcResult.GerarXML().OuterXml;
+                var byteData = System.Text.Encoding.UTF8.GetBytes(value);
+                stream.Write(byteData, 0, byteData.Length);
+                stream.Close();
+            }
+            catch (Exception ex)
+            {
+                ThrowHelper.Instance.Throw(ex);
+            }
+        }
+
+        #endregion Public Methods
     }
 }
