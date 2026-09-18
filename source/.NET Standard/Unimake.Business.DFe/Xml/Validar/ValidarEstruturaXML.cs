3e19fcabf0045631ab15e1503cf69e1d61a08f1e
Eduardo Cavazin <eduardotorcav@gmail.com>
2026-09-18 15:43:40 -0300

    feat: reimplementa versão 2.04 do padrão ISSNET em Santa Maria - RS ID #187882

--- source/.NET Standard/Unimake.Business.DFe/Xml/Validar/ValidarEstruturaXML.cs
+++ source/.NET Standard/Unimake.Business.DFe/Xml/Validar/ValidarEstruturaXML.cs
@@ -1694,8 +1694,36 @@ namespace Unimake.Business.DFe
                     return RaizEh("nfse", "nota") ? "1.20" : "2.04";
 
                 case PadraoNFSe.ISSNET:
-                    return codigoMunicipio == 4316907 && versaoDeclarada == "1.00"
-                        ? "1.00"
+                    if (codigoMunicipio == 4316907 && versaoDeclarada == "1.00")
+                    {
+                        return "1.00";
+                    }
+
+                    if (namespaceRaiz.IndexOf("sped.fazenda.gov.br/nfse", StringComparison.OrdinalIgnoreCase) >= 0)
+                    {
+                        return "1.01";
+                    }
+
+                    if (namespaceRaiz.IndexOf("abrasf.org.br/nfse.xsd", StringComparison.OrdinalIgnoreCase) >= 0)
+                    {
+                        return "2.04";
+                    }
+
+                    if (RaizEh("DPS", "NFSe", "pedRegEvento"))
+                    {
+                        return "1.01";
+                    }
+
+                    return Contem("Pedido") ||
+                           Contem("InfDeclaracaoPrestacaoServico") ||
+                           RaizEh(
+                               "ConsultarLoteRpsEnvio",
+                               "ConsultarNfseRpsEnvio",
+                               "ConsultarRpsDisponivelEnvio",
+                               "EnviarLoteRpsEnvio",
+                               "EnviarLoteRpsSincronoEnvio",
+                               "SubstituirNfseEnvio")
+                        ? "2.04"
                         : "1.01";
 
                 case PadraoNFSe.PAULISTANA:
